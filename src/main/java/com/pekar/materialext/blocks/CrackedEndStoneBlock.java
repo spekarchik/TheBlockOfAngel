bce4b962c319334b0216244a262e3b2ec1321c6f
PEKAR\Sergey <sergey.pekarchik@gmail.com>
2021-12-12 05:01:46 +0300

    added CrackedEndStoneBlock

--- src/main/java/com/pekar/materialext/blocks/CrackedEndStoneBlock.java
+++ src/main/java/com/pekar/materialext/blocks/CrackedEndStoneBlock.java
@@ -1,43 +1,27 @@
 package com.pekar.materialext.blocks;
 
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.LevelReader;
 import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.material.Material;
+
+import java.util.Random;
 
 public class CrackedEndStoneBlock extends ModBlock
 {
     public CrackedEndStoneBlock()
     {
         super(BlockBehaviour.Properties.of(Material.SNOW)
-                        .strength(0.5f)
-                        .sound(SoundType.SNOW));
-//        setHardness(0.5f);
-//        setHarvestLevel("shovel", 2);
+                .strength(0.5f)
+                .sound(SoundType.SNOW)
+                .requiresCorrectToolForDrops());
     }
 
-//    @Override
-//    public Item getItemDropped(IBlockState state, Random rand, int fortune)
-//    {
-//        if (fortune >= 20) return ItemRegistry.ENDSTONE_POWDER;
-//
-//        if (fortune > 3)
-//        {
-//            fortune = 3;
-//        }
-//
-//        return rand.nextInt(12 - fortune * 3) == 0 ? ItemRegistry.ENDSTONE_POWDER : Items.AIR;
-//    }
-//
-//    @Override
-//    public int getExpDrop(IBlockState state, IBlockAccess world, BlockPos pos, int fortune)
-//    {
-//        Random rand = world instanceof World ? ((World)world).rand : new Random();
-//        return MathHelper.getInt(rand, 2, 5);
-//    }
-//
-//    @Override
-//    public int quantityDropped(Random random)
-//    {
-//        return random.nextInt(4) + 1;
-//    }
+    @Override
+    public int getExpDrop(BlockState state, LevelReader world, BlockPos pos, int fortune, int silktouch)
+    {
+        return new Random().nextInt(2,5);
+    }
 }
