bdb8cc834f523a152f0325ebbf17ffa3cfd3e353
PEKAR\Sergey <sergey.pekarchik@gmail.com>
2022-01-25 01:39:43 +0300

    correction

--- src/main/java/com/pekar/angelblock/Main.java
+++ src/main/java/com/pekar/angelblock/Main.java
@@ -3,6 +3,8 @@ package com.pekar.angelblock;
 import com.pekar.angelblock.armor.ArmorRegistry;
 import com.pekar.angelblock.blocks.BlockRegistry;
 import com.pekar.angelblock.blocks.tile_entities.EntityRegistry;
+import com.pekar.angelblock.events.ClientSetupEvents;
+import com.pekar.angelblock.events.ClientTickEvents;
 import com.pekar.angelblock.events.EventRegistry;
 import com.pekar.angelblock.items.ItemRegistry;
 import com.pekar.angelblock.network.PacketHandler;
@@ -64,6 +66,8 @@ public class Main
         ItemRegistry.initStatic();
         EntityRegistry.initStatic();
         ArmorRegistry.initStatic();
+        ClientSetupEvents.initStatic();
+        ClientTickEvents.initStatic();
         PacketHandler.init();
     }
 
--- src/main/java/com/pekar/angelblock/events/ClientSetupEvents.java
+++ src/main/java/com/pekar/angelblock/events/ClientSetupEvents.java
@@ -3,7 +3,6 @@ package com.pekar.angelblock.events;
 import com.pekar.angelblock.Main;
 import com.pekar.angelblock.keybinds.KeyRegistry;
 import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
@@ -14,10 +13,14 @@ public final class ClientSetupEvents
     private ClientSetupEvents()
     {}
 
+    public static void initStatic()
+    {
+        // do nothing
+    }
+
     @SubscribeEvent
     public static void onClientSetup(FMLClientSetupEvent event)
     {
         KeyRegistry.registerKeys();
-        MinecraftForge.EVENT_BUS.register(new ClientTickEvents());
     }
 }
