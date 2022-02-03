2fbf0eb7e51ff62c0db76a77fd48de952f85f69e
PEKAR\Sergey <sergey.pekarchik@gmail.com>
2022-02-03 22:36:38 +0300

    flying armor effects improvements

--- src/main/java/com/pekar/angelblock/events/armor/FlyingArmor.java
+++ src/main/java/com/pekar/angelblock/events/armor/FlyingArmor.java
@@ -4,6 +4,8 @@ import com.pekar.angelblock.armor.ArmorRegistry;
 import com.pekar.angelblock.events.effect.*;
 import com.pekar.angelblock.events.player.IPlayer;
 import com.pekar.angelblock.keybinds.KeyRegistry;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.world.level.Level;
 import net.minecraftforge.event.entity.EntityTravelToDimensionEvent;
 import net.minecraftforge.event.entity.living.*;
 import net.minecraftforge.event.entity.player.PlayerEvent;
@@ -12,12 +14,14 @@ public class FlyingArmor extends Armor
 {
     private final SwitchingEffectSynchronizer jumpBoostEffect;
     private final IArmorEffect slowFallingEffect;
+    private final IArmorEffect levitationEffect;
 
     public FlyingArmor(IPlayer player)
     {
         super(player);
 
-        slowFallingEffect = new SlowFallingPermanentArmorEffect(player, this);
+        slowFallingEffect = new SlowFallingSwitchingEffect(player, this);
+        levitationEffect = new LevitationTemporaryEffect(player, this, 1, 200);
 
         var speedEffect = new SpeedSwitchingEffect(player, this, 1);
         var jumpBoostEffect = new JumpBoostArmorEffect(player, this, 24);
@@ -53,6 +57,8 @@ public class FlyingArmor extends Armor
     public void onPlayerLoggedInEvent(PlayerEvent.PlayerLoggedInEvent event)
     {
         jumpBoostEffect.updateSwitchState();
+        levitationEffect.updateSwitchState();
+        slowFallingEffect.updateSwitchState();
     }
 
     @Override
@@ -70,6 +76,7 @@ public class FlyingArmor extends Armor
     {
         jumpBoostEffect.updateEffectAvailability();
         slowFallingEffect.updateEffectAvailability();
+        levitationEffect.updateEffectAvailability();
 
         updatePotionEffects();
     }
@@ -89,18 +96,37 @@ public class FlyingArmor extends Armor
     {
         if (pressedKeyDescription.equals(KeyRegistry.JUMP_BOOST.getName()))
         {
-            jumpBoostEffect.trySwitch();
+            if (player.isOverworld())
+            {
+                jumpBoostEffect.trySwitch();
+            }
+            else if (player.isEnd())
+            {
+                levitationEffect.trySwitchOn();
+            }
         }
     }
 
     @Override
     public void onEntityTravelToDimensionEvent(EntityTravelToDimensionEvent event)
     {
+        var destDimension = event.getDimension();
+        if (isNether(destDimension))
+        {
+            slowFallingEffect.trySwitchOff();
+            jumpBoostEffect.trySwitchOff();
+            levitationEffect.trySwitchOff();
+        }
+        else
+        {
+            slowFallingEffect.trySwitchOn();
+        }
     }
 
     @Override
     public void onBreakSpeed(PlayerEvent.BreakSpeed event)
     {
+        event.setNewSpeed(0);
     }
 
     @Override
@@ -117,5 +143,21 @@ public class FlyingArmor extends Armor
     {
         jumpBoostEffect.updateEffectActivity();
         slowFallingEffect.updateEffectActivity();
+        levitationEffect.updateEffectActivity();
+    }
+
+    private boolean isOverworld(ResourceKey<Level> dimension)
+    {
+        return dimension.location().equals(Level.OVERWORLD.location());
+    }
+
+    private boolean isNether(ResourceKey<Level> dimension)
+    {
+        return dimension.location().equals(Level.NETHER.location());
+    }
+
+    private boolean isEnd(ResourceKey<Level> dimension)
+    {
+        return dimension.location().equals(Level.END.location());
     }
 }
---
+++ src/main/java/com/pekar/angelblock/events/effect/LevitationTemporaryEffect.java
@@ -0,0 +1,19 @@
+package com.pekar.angelblock.events.effect;
+
+import com.pekar.angelblock.events.armor.IArmor;
+import com.pekar.angelblock.events.player.IPlayer;
+import net.minecraft.world.effect.MobEffects;
+
+public class LevitationTemporaryEffect extends TemporaryArmorEffect
+{
+    public LevitationTemporaryEffect(IPlayer player, IArmor armor, int defaultAmplifier, int duration)
+    {
+        super(player, armor, MobEffects.LEVITATION, defaultAmplifier, duration);
+    }
+
+    @Override
+    protected boolean isAvailable()
+    {
+        return player.isFullArmorSetPutOn(armor.getArmorElementNames());
+    }
+}
