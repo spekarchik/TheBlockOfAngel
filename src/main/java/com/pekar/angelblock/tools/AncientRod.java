f47b6640e2bfbe48a180f22fc84f3bc651f4ccb6
PEKAR\Sergey <sergey.pekarchik@gmail.com>
2022-02-14 12:55:00 +0300

    updated rods

--- src/main/java/com/pekar/angelblock/tools/AncientRod.java
+++ src/main/java/com/pekar/angelblock/tools/AncientRod.java
@@ -4,20 +4,21 @@ import com.pekar.angelblock.blocks.BlockRegistry;
 import com.pekar.angelblock.network.packets.PlaySoundPacket;
 import com.pekar.angelblock.network.packets.SoundType;
 import com.pekar.angelblock.potions.PotionRegistry;
+import net.minecraft.advancements.CriteriaTriggers;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Tier;
 import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.item.context.UseOnContext;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.Blocks;
-import net.minecraft.world.level.block.InfestedBlock;
-import net.minecraft.world.level.block.LeavesBlock;
+import net.minecraft.world.level.block.*;
 import net.minecraft.world.level.block.state.BlockState;
 
+import java.util.Random;
 import java.util.function.BiFunction;
 
 public class AncientRod extends MagneticRod
@@ -83,6 +84,72 @@ public class AncientRod extends MagneticRod
                 damageItemIfSurvival(player, level, pos, blockState);
                 return InteractionResult.CONSUME;
             }
+
+            var hand = context.getHand();
+            var facing = context.getClickedFace();
+            var itemRand = new Random();
+
+            if (facing == Direction.UP)
+            {
+                if (Utils.isNearWaterHorizoltal(level, pos) && (block == Blocks.DIRT || block == Blocks.COARSE_DIRT
+                        || block == Blocks.GRASS_BLOCK || block == Blocks.PODZOL || block instanceof SandBlock
+                        || block == Blocks.MOSS_BLOCK || block == Blocks.MYCELIUM))
+                {
+                    damageItemIfSurvival(player, level, pos, blockState);
+                    return plant(player, level, pos, hand, facing, Blocks.SUGAR_CANE);
+                }
+
+                if (block == Blocks.DIRT || block == Blocks.COARSE_DIRT || block == Blocks.MOSS_BLOCK)
+                {
+                    damageItemIfSurvival(player, level, pos, blockState);
+                    int randomValue = itemRand.nextInt() % 3;
+
+                    switch (randomValue)
+                    {
+                        case 0:
+                            return plant(player, level, pos, hand, facing, Blocks.SWEET_BERRY_BUSH);
+                        case 1:
+                            return plant(player, level, pos, hand, facing, Blocks.FERN);
+                        default:
+                        {
+                            int random = itemRand.nextInt() & 2;
+                            Block plantBlock = random > 0 ? Blocks.AZALEA : Blocks.FLOWERING_AZALEA;
+                            return plant(player, level, pos, hand, facing, plantBlock);
+                        }
+                    }
+                }
+
+                if (block == Blocks.GRASS_BLOCK)
+                {
+                    damageItemIfSurvival(player, level, pos, blockState);
+                    int randomValue = itemRand.nextInt() % 16;
+                    return plant(player, level, pos, hand, facing, chooseFlowerByValue(randomValue));
+                }
+
+                if (block == Blocks.SAND)
+                {
+                    damageItemIfSurvival(player, level, pos, blockState);
+                    return plant(player, level, pos, hand, facing, Blocks.CACTUS);
+                }
+
+                if (block == Blocks.FARMLAND)
+                {
+                    damageItemIfSurvival(player, level, pos, blockState);
+                    int randomValue = itemRand.nextInt() % 4;
+
+                    switch (randomValue)
+                    {
+                        case 0:
+                            return plant(player, level, pos, hand, facing, Blocks.WHEAT);
+                        case 1:
+                            return plant(player, level, pos, hand, facing, Blocks.POTATOES);
+                        case 2:
+                            return plant(player, level, pos, hand, facing, Blocks.CARROTS);
+                        default:
+                            return plant(player, level, pos, hand, facing, Blocks.BEETROOTS);
+                    }
+                }
+            }
         }
 
         return InteractionResult.PASS;
@@ -146,5 +213,50 @@ public class AncientRod extends MagneticRod
         }
 
         return InteractionResult.PASS;
+    }
+
+    protected InteractionResult plant(Player player, Level level, BlockPos pos, InteractionHand hand, Direction facing, Block plantBlock)
+    {
+        var itemStack = player.getItemInHand(hand);
+
+        if (facing == Direction.UP && level.isEmptyBlock(pos.above()))
+        {
+            level.setBlock(pos.above(), plantBlock.defaultBlockState(), 11);
+
+            if (player instanceof ServerPlayer serverPlayer)
+            {
+                new PlaySoundPacket(SoundType.PLANT).sendToPlayer(serverPlayer);
+                CriteriaTriggers.PLACED_BLOCK.trigger(serverPlayer, pos.above(), itemStack);
+            }
+
+            return InteractionResult.CONSUME;
+        }
+        else
+        {
+            return InteractionResult.FAIL;
+        }
+    }
+
+    private Block chooseFlowerByValue(int value)
+    {
+        switch (value)
+        {
+            case 1: return Blocks.POPPY;
+            case 2: return Blocks.BLUE_ORCHID;
+            case 3: return Blocks.ALLIUM;
+            case 4: return Blocks.AZURE_BLUET;
+            case 5: return Blocks.RED_TULIP;
+            case 6: return Blocks.ORANGE_TULIP;
+            case 7: return Blocks.WHITE_TULIP;
+            case 8: return Blocks.PINK_TULIP;
+            case 9: return Blocks.OXEYE_DAISY;
+            case 10: return Blocks.CORNFLOWER;
+            case 11: return Blocks.LILY_OF_THE_VALLEY;
+            case 12: return Blocks.SUNFLOWER;
+            case 13: return Blocks.LILAC;
+            case 14: return Blocks.ROSE_BUSH;
+            case 15: return Blocks.PEONY;
+            default: return Blocks.DANDELION;
+        }
     }
 }
--- src/main/java/com/pekar/angelblock/tools/MarineRod.java
+++ src/main/java/com/pekar/angelblock/tools/MarineRod.java
@@ -3,19 +3,13 @@ package com.pekar.angelblock.tools;
 import com.pekar.angelblock.network.packets.PlaySoundPacket;
 import com.pekar.angelblock.network.packets.SoundType;
 import com.pekar.angelblock.potions.PotionRegistry;
-import net.minecraft.advancements.CriteriaTriggers;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.server.level.ServerPlayer;
-import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
-import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Tier;
 import net.minecraft.world.item.context.UseOnContext;
-import net.minecraft.world.level.Level;
-import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
-import net.minecraft.world.level.block.SandBlock;
 import net.minecraft.world.level.block.state.BlockState;
 
 import java.util.Random;
@@ -35,6 +29,7 @@ public class MarineRod extends AncientRod
 
         if (level.isClientSide) return InteractionResult.PASS;
         if (!canUseToolEffect(player)) return InteractionResult.PASS;
+
         if (isEnhancedRod() && player.hasEffect(PotionRegistry.ROD_MAGNETIC_MODE_EFFECT.get()))
             return super.useOn(context);
 
@@ -45,12 +40,12 @@ public class MarineRod extends AncientRod
         var itemStack = player.getItemInHand(context.getHand());
         boolean isBroken = itemStack.getMaxDamage() - itemStack.getDamageValue() <= 1;
 
+        var hand = context.getHand();
+        var facing = context.getClickedFace();
+        var itemRand = new Random();
+
         if (!isBroken)
         {
-            var hand = context.getHand();
-            var facing = context.getClickedFace();
-            var itemRand = new Random();
-
             if (facing == Direction.UP)
             {
                 BlockPos upPos = pos.above();
@@ -67,80 +62,6 @@ public class MarineRod extends AncientRod
 
                     return InteractionResult.CONSUME;
                 }
-
-                if (Utils.isNearWaterHorizoltal(level, pos) && (block == Blocks.DIRT || block == Blocks.COARSE_DIRT
-                        || block == Blocks.GRASS_BLOCK || block == Blocks.PODZOL || block instanceof SandBlock
-                        || block == Blocks.MOSS_BLOCK || block == Blocks.MYCELIUM))
-                {
-                    damageItemIfSurvival(player, level, pos, blockState);
-                    return plant(player, level, pos, hand, facing, Blocks.SUGAR_CANE);
-                }
-
-                if (block == Blocks.FARMLAND)
-                {
-                    damageItemIfSurvival(player, level, pos, blockState);
-                    int randomValue = itemRand.nextInt() % 4;
-
-                    switch (randomValue)
-                    {
-                        case 0:
-                            return plant(player, level, pos, hand, facing, Blocks.WHEAT);
-                        case 1:
-                            return plant(player, level, pos, hand, facing, Blocks.POTATOES);
-                        case 2:
-                            return plant(player, level, pos, hand, facing, Blocks.CARROTS);
-                        default:
-                            return plant(player, level, pos, hand, facing, Blocks.BEETROOTS);
-                    }
-                }
-
-                if (block == Blocks.DIRT || block == Blocks.COARSE_DIRT || block == Blocks.MOSS_BLOCK)
-                {
-                    damageItemIfSurvival(player, level, pos, blockState);
-                    int randomValue = itemRand.nextInt() % 3;
-
-                    switch (randomValue)
-                    {
-                        case 0:
-                            return plant(player, level, pos, hand, facing, Blocks.SWEET_BERRY_BUSH);
-                        case 1:
-                            return plant(player, level, pos, hand, facing, Blocks.FERN);
-                        default:
-                        {
-                            int random = itemRand.nextInt() & 2;
-                            Block plantBlock = random > 0 ? Blocks.AZALEA : Blocks.FLOWERING_AZALEA;
-                            return plant(player, level, pos, hand, facing, plantBlock);
-                        }
-                    }
-                }
-
-                if (block == Blocks.GRASS_BLOCK)
-                {
-                    damageItemIfSurvival(player, level, pos, blockState);
-                    int randomValue = itemRand.nextInt() % 16;
-                    return plant(player, level, pos, hand, facing, chooseFlowerByValue(randomValue));
-                }
-
-                if (block == Blocks.SAND)
-                {
-                    damageItemIfSurvival(player, level, pos, blockState);
-                    return plant(player, level, pos, hand, facing, Blocks.CACTUS);
-                }
-
-                if (block == Blocks.PODZOL || block == Blocks.MYCELIUM)
-                {
-                    damageItemIfSurvival(player, level, pos, blockState);
-                    int randomValue = itemRand.nextInt() & 1;
-                    var plantBlock = randomValue == 0 ? Blocks.BROWN_MUSHROOM : Blocks.RED_MUSHROOM;
-                    return plant(player, level, pos, hand, facing, plantBlock);
-                }
-
-                if (block == Blocks.RED_SAND || block == Blocks.GRAVEL)
-                {
-                    damageItemIfSurvival(player, level, pos, blockState);
-                    return plant(player, level, pos, hand, facing, Blocks.BAMBOO);
-                }
-
             }
 
             if (block == Blocks.MELON)
@@ -151,57 +72,32 @@ public class MarineRod extends AncientRod
             }
         }
 
-        return super.useOn(context);
-    }
+        var result = super.useOn(context);
+        if (result.shouldAwardStats()) return result;
 
-    protected InteractionResult plant(Player player, Level level, BlockPos pos, InteractionHand hand, Direction facing, Block plantBlock)
-    {
-        var itemStack = player.getItemInHand(hand);
-
-        if (facing == Direction.UP && level.isEmptyBlock(pos.above()))
+        if (!isBroken)
         {
-            level.setBlock(pos.above(), plantBlock.defaultBlockState(), 11);
-
-            if (player instanceof ServerPlayer serverPlayer)
+            if (block == Blocks.PODZOL || block == Blocks.MYCELIUM)
             {
-                new PlaySoundPacket(SoundType.PLANT).sendToPlayer(serverPlayer);
-                CriteriaTriggers.PLACED_BLOCK.trigger(serverPlayer, pos.above(), itemStack);
+                damageItemIfSurvival(player, level, pos, blockState);
+                int randomValue = itemRand.nextInt() & 1;
+                var plantBlock = randomValue == 0 ? Blocks.BROWN_MUSHROOM : Blocks.RED_MUSHROOM;
+                return plant(player, level, pos, hand, facing, plantBlock);
             }
 
-            return InteractionResult.CONSUME;
+            if (block == Blocks.RED_SAND || block == Blocks.GRAVEL)
+            {
+                damageItemIfSurvival(player, level, pos, blockState);
+                return plant(player, level, pos, hand, facing, Blocks.BAMBOO);
+            }
         }
-        else
-        {
-            return InteractionResult.FAIL;
-        }
+
+        return InteractionResult.PASS;
     }
 
     @Override
     protected int getShiftDepth()
     {
         return 7;
-    }
-
-    private Block chooseFlowerByValue(int value)
-    {
-        switch (value)
-        {
-            case 1: return Blocks.POPPY;
-            case 2: return Blocks.BLUE_ORCHID;
-            case 3: return Blocks.ALLIUM;
-            case 4: return Blocks.AZURE_BLUET;
-            case 5: return Blocks.RED_TULIP;
-            case 6: return Blocks.ORANGE_TULIP;
-            case 7: return Blocks.WHITE_TULIP;
-            case 8: return Blocks.PINK_TULIP;
-            case 9: return Blocks.OXEYE_DAISY;
-            case 10: return Blocks.CORNFLOWER;
-            case 11: return Blocks.LILY_OF_THE_VALLEY;
-            case 12: return Blocks.SUNFLOWER;
-            case 13: return Blocks.LILAC;
-            case 14: return Blocks.ROSE_BUSH;
-            case 15: return Blocks.PEONY;
-            default: return Blocks.DANDELION;
-        }
     }
 }
