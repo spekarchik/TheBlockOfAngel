b9b6ef31757d0dc173e16811d19fa3312014a97d
PEKAR\Sergey <sergey.pekarchik@gmail.com>
2022-07-19 22:56:35 +0200

    cleanup

--- src/main/java/com/pekar/angelblock/armor/ModArmor.java
+++ src/main/java/com/pekar/angelblock/armor/ModArmor.java
@@ -2,21 +2,15 @@ package com.pekar.angelblock.armor;
 
 import com.pekar.angelblock.Main;
 import com.pekar.angelblock.tab.ModTab;
-import net.minecraft.resources.ResourceKey;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.item.ArmorItem;
 import net.minecraft.world.item.ArmorMaterial;
-import net.minecraft.world.level.Level;
 import net.minecraftforge.registries.RegistryObject;
 
-import java.util.HashSet;
-import java.util.Set;
 import java.util.function.Supplier;
 
 public class ModArmor extends ArmorItem
 {
-    public static final Set<String> ArmorNames = new HashSet<>();
-
     protected ModArmor(ArmorMaterial material, EquipmentSlot equipmentSlot)
     {
         super(material, equipmentSlot, new Properties().tab(ModTab.MOD_TAB));
@@ -24,7 +18,6 @@ public class ModArmor extends ArmorItem
 
     protected static RegistryObject<ArmorItem> register(String name, Supplier<ArmorItem> sup)
     {
-        ArmorNames.add(name);
         return Main.ITEMS.register(name, sup);
     }
 }
