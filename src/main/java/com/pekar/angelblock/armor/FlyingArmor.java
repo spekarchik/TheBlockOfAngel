3b03f8442b9c542af6a42ad530f8589d6ea817e2
PEKAR\Sergey <sergey.pekarchik@gmail.com>
2022-09-19 22:07:02 +0200

    cleanup

--- src/main/java/com/pekar/angelblock/armor/FlyingArmor.java
+++ src/main/java/com/pekar/angelblock/armor/FlyingArmor.java
@@ -5,7 +5,6 @@ import net.minecraft.world.InteractionHand;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.ArmorMaterial;
-import net.minecraft.world.item.CompassItem;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
 
