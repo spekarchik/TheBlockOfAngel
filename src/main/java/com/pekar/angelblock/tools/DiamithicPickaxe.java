5619a9e76123c0ad559bd19c5992e4d67e7581a0
PEKAR\Sergey <sergey.pekarchik@gmail.com>
2023-03-03 02:03:49 +0100

    pickaxes code optimization

--- src/main/java/com/pekar/angelblock/tools/DiamithicPickaxe.java
+++ src/main/java/com/pekar/angelblock/tools/DiamithicPickaxe.java
@@ -3,7 +3,6 @@ package com.pekar.angelblock.tools;
 import com.pekar.angelblock.tools.properties.DiamithicMaterialProperties;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Tier;
 import net.minecraft.world.level.Level;
@@ -14,13 +13,6 @@ public class DiamithicPickaxe extends ModPickaxe
     public DiamithicPickaxe(Tier material, int attackDamage, float attackSpeed, Properties properties)
     {
         super(material, attackDamage, attackSpeed, properties, new DiamithicMaterialProperties());
-    }
-
-    @Override
-    public boolean onBlockStartBreak(ItemStack itemstack, BlockPos pos, Player player)
-    {
-        if (canPreventBlockDestroying(player, pos) && !materialProperties.isSafeToBreak(player, pos)) return true;
-        return super.onBlockStartBreak(itemstack, pos, player);
     }
 
     @Override
