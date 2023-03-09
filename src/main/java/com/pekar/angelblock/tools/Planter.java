7f8cda4ad122b2d4097c84c18a2d09ae0f7ccee8
PEKAR\Sergey <sergey.pekarchik@gmail.com>
2023-03-10 00:26:03 +0100

    planter: added bonemealing function

--- src/main/java/com/pekar/angelblock/tools/Planter.java
+++ src/main/java/com/pekar/angelblock/tools/Planter.java
@@ -2,11 +2,13 @@ package com.pekar.angelblock.tools;
 
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
+import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
 import net.minecraft.world.item.Tier;
 import net.minecraft.world.item.context.UseOnContext;
 import net.minecraft.world.level.Level;
@@ -24,15 +26,22 @@ public class Planter extends ModRod
     {
         var player = context.getPlayer();
         var level = player.level;
-
+        var offHandItemStack = player.getItemInHand(InteractionHand.OFF_HAND);
         var pos = context.getClickedPos();
-        return processBlocks(player, level, pos, context.getClickedFace())
-                ? InteractionResult.sidedSuccess(level.isClientSide)
-                : InteractionResult.PASS;
 
+        if (offHandItemStack.getItem() == Items.BONE_MEAL)
+        {
+            bonemealPlants(player, level, pos, context.getClickedFace());
+        }
+        else
+        {
+            plantOffHandItems(player, level, pos, context.getClickedFace());
+        }
+
+        return InteractionResult.sidedSuccess(level.isClientSide());
     }
 
-    protected boolean processBlocks(Player player, Level level, BlockPos pos, Direction facing)
+    protected boolean plantOffHandItems(Player player, Level level, BlockPos pos, Direction facing)
     {
         if (facing != Direction.UP) return false;
 
@@ -71,7 +80,49 @@ public class Planter extends ModRod
         for (int x = posX; x != posX + shiftX; x += increment)
             for (int z = posZ; z != posZ + shiftZ; z += increment)
             {
-                boolean hasTransformed = onBlockProcessing(player, level, originBlock, new BlockPos(x, posY, z), facing, toolItemStack, plantableBlock);
+                boolean hasTransformed = plantOffHandItem(player, level, originBlock, new BlockPos(x, posY, z), facing, toolItemStack, plantableBlock);
+                if (hasTransformed)
+                    haveAnyTransformed = true;
+                else
+                    return haveAnyTransformed;
+            }
+
+        return haveAnyTransformed;
+    }
+
+    protected boolean bonemealPlants(Player player, Level level, BlockPos pos, Direction facing)
+    {
+        var bonemealInHand = player.getItemInHand(InteractionHand.OFF_HAND);
+        if (bonemealInHand.getItem() != Items.BONE_MEAL) return false;
+
+        final int MAX_BONEMEALABLE_LENGTH = 64;
+        final int posX = pos.getX(), posY = pos.getY(), posZ = pos.getZ();
+
+        int shiftX = 0, shiftZ = 0, increment = 0;
+
+        switch (player.getDirection())
+        {
+            case NORTH:
+                shiftX = -1; shiftZ = -MAX_BONEMEALABLE_LENGTH; increment = -1; break;
+
+            case SOUTH:
+                shiftX = 1; shiftZ = MAX_BONEMEALABLE_LENGTH; increment = 1; break;
+
+            case EAST:
+                shiftX = MAX_BONEMEALABLE_LENGTH; shiftZ = 1; increment = 1; break;
+
+            case WEST:
+                shiftX = -MAX_BONEMEALABLE_LENGTH; shiftZ = -1; increment = -1; break;
+        }
+
+        boolean haveAnyTransformed = false;
+        var toolItemStack = player.getItemInHand(InteractionHand.MAIN_HAND);
+        var originBlock = level.getBlockState(pos).getBlock();
+
+        for (int x = posX; x != posX + shiftX; x += increment)
+            for (int z = posZ; z != posZ + shiftZ; z += increment)
+            {
+                boolean hasTransformed = bonemealPlant(player, level, originBlock, new BlockPos(x, posY, z), facing, toolItemStack);
                 if (hasTransformed)
                     haveAnyTransformed = true;
                 else
@@ -89,7 +140,7 @@ public class Planter extends ModRod
                 || block == Blocks.SUGAR_CANE;
     }
 
-    private boolean onBlockProcessing(Player player, Level level, Block originBlock, BlockPos pos, Direction facing, ItemStack toolItemStack, Block plantBlock)
+    private boolean plantOffHandItem(Player player, Level level, Block originBlock, BlockPos pos, Direction facing, ItemStack toolItemStack, Block plantBlock)
     {
         boolean isBroken = toolItemStack.getMaxDamage() - toolItemStack.getDamageValue() <= 1;
         if (isBroken) return false;
@@ -111,5 +162,36 @@ public class Planter extends ModRod
         }
 
         return result.shouldAwardStats();
+    }
+
+    private boolean bonemealPlant(Player player, Level level, Block originBlock, BlockPos pos, Direction facing, ItemStack toolItemStack)
+    {
+        boolean isBroken = toolItemStack.getMaxDamage() - toolItemStack.getDamageValue() <= 1;
+        if (isBroken) return false;
+
+        var blockState = level.getBlockState(pos);
+        Block block = blockState.getBlock();
+
+        if (!(block instanceof BonemealableBlock bonemealableBlock)) return false;
+
+        var itemStack = player.getItemInHand(InteractionHand.OFF_HAND);
+        int itemCount = itemStack.getCount();
+        if (itemCount < 1) return false;
+
+        if (bonemealableBlock.isValidBonemealTarget(level, pos, blockState, true))
+        {
+            if (!level.isClientSide())
+            {
+                bonemealableBlock.performBonemeal((ServerLevel) level, level.random, pos, blockState);
+
+                if (bonemealableBlock.isBonemealSuccess(level, level.random, pos, blockState))
+                {
+                    damageItemIfSurvival(player, level, pos.below(), level.getBlockState(pos.below()));
+                    itemStack.setCount(itemCount - 1);
+                }
+            }
+        }
+
+        return true;
     }
 }
