a08c057b1dd614d9859e672f3233ff02679dac01
PEKAR\Sergey <sergey.pekarchik@gmail.com>
2023-06-25 16:47:42 +0200

    added common descriptions for armor

--- src/main/java/com/pekar/angelblock/armor/ModArmor.java
+++ src/main/java/com/pekar/angelblock/armor/ModArmor.java
@@ -1,11 +1,21 @@
 package com.pekar.angelblock.armor;
 
+import com.pekar.angelblock.Utils;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.MutableComponent;
 import net.minecraft.world.item.ArmorItem;
 import net.minecraft.world.item.ArmorMaterial;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.level.Level;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
 
 public class ModArmor extends ArmorItem
 {
     protected final String armorModelName;
+    protected final Utils utils = new Utils();
 
     protected ModArmor(ArmorMaterial material, Type equipmentSlot, String armorModelName)
     {
@@ -41,5 +51,53 @@ public class ModArmor extends ArmorItem
     public boolean isModifiedWithSeaPower()
     {
         return false;
+    }
+
+    @Override
+    public void appendHoverText(ItemStack itemStack, @Nullable Level level, List<Component> components, TooltipFlag tooltipFlag)
+    {
+        for (int i = 1; i <= 9; i++)
+        {
+            components.add(getCommonDescription(i, i == 4, false, i == 3, false));
+        }
+
+        for (int i = 1; i <= getDescriptionLineCount(); i++)
+        {
+//            components.add(getSpecificDescription(i, i == 1, false, false, false));
+        }
+    }
+
+    private int getDescriptionLineCount()
+    {
+        return switch (getEquipmentSlot())
+        {
+            case HEAD -> 4;
+            case CHEST -> 7;
+            case LEGS -> 5;
+            case FEET -> 6;
+            default -> 0;
+        };
+    }
+
+    private MutableComponent getCommonDescription(int lineNumber, boolean isHeader, boolean isSubHeader, boolean isNotice, boolean isImportantNotice)
+    {
+        var component = getCommonDisplayName(lineNumber);
+        return utils.getFormattedTextComponent(component, isHeader, isSubHeader, isNotice, isImportantNotice);
+    }
+
+    private MutableComponent getSpecificDescription(int lineNumber, boolean isHeader, boolean isSubHeader, boolean isNotice, boolean isImportantNotice)
+    {
+        var component = getSpecificDisplayName(lineNumber);
+        return utils.getFormattedTextComponent(component, isHeader, isSubHeader, isNotice, isImportantNotice);
+    }
+
+    private MutableComponent getSpecificDisplayName(int lineNumber)
+    {
+        return Component.translatable(this.getDescriptionId() + ".desc" + lineNumber);
+    }
+
+    private MutableComponent getCommonDisplayName(int lineNumber)
+    {
+        return Component.translatable(material.getName().replace(':', '.').replaceAll("[0-9]", "") + ".desc" + lineNumber);
     }
 }
