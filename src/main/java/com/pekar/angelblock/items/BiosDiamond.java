c2a07c8b3ba8c2da237ab7973b288795e0b49096
PEKAR\Sergey <sergey.pekarchik@gmail.com>
2023-07-15 17:53:13 +0200

    added a new description for bios diamond

--- src/main/java/com/pekar/angelblock/items/BiosDiamond.java
+++ src/main/java/com/pekar/angelblock/items/BiosDiamond.java
@@ -1,12 +1,18 @@
 package com.pekar.angelblock.items;
 
+import net.minecraft.ChatFormatting;
+import net.minecraft.network.chat.Component;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.level.Level;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
 
 public class BiosDiamond extends ModItemWithDoubleHoverText
 {
@@ -20,5 +26,17 @@ public class BiosDiamond extends ModItemWithDoubleHoverText
         }
 
         return InteractionResultHolder.pass(player.getItemInHand(interactionHand));
+    }
+
+    @Override
+    public void appendHoverText(ItemStack itemStack, @Nullable Level level, List<Component> components, TooltipFlag tooltipFlag)
+    {
+        for (int i = 1; i <= 3; i++)
+        {
+            var component = getDisplayName(i).withStyle(ChatFormatting.GRAY);
+            if (i > 1)
+                component.withStyle(ChatFormatting.ITALIC);
+            components.add(component);
+        }
     }
 }
