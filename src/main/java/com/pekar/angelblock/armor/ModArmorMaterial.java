6eac3487ba33dc5ec9131e263cbb6b023b4ad0c9
PEKAR\Sergey <sergey.pekarchik@gmail.com>
2024-06-23 20:49:30 +0200

    updating...

--- src/main/java/com/pekar/angelblock/armor/ModArmorMaterial.java
+++ src/main/java/com/pekar/angelblock/armor/ModArmorMaterial.java
@@ -10,81 +10,90 @@ import net.minecraft.world.item.ArmorMaterial;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.item.crafting.Ingredient;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.function.Supplier;
 
-public class ModArmorMaterial implements ArmorMaterial
+import static net.minecraft.world.item.ArmorItem.Type.*;
+
+public class ModArmorMaterial extends ArmorMaterial
 {
     private final String name;
-    private final int durabilityMultiplier;
     private final int[] slotProtections;
     private final int enchantmentValue;
     private final SoundEvent sound;
     private final float toughness;
     private final float knockbackResistance;
     private final Supplier<Ingredient> repairIngredient;
-    private static final int[] HEALTH_PER_SLOT = new int[]{13, 15, 16, 11};
 
-    protected static final Holder<ArmorMaterial> RENDELITHIC = new ModArmorMaterial("angelblock:rendelithic_armor",
+    private static final Map<ArmorItem.Type, Integer> DEFENSE = new HashMap<>();
+
+    private static final List<Layer> RendelithicLayers = 
+
+    static
+    {
+        DEFENSE.put(HELMET, 11);
+        DEFENSE.put(CHESTPLATE, 16);
+        DEFENSE.put(LEGGINGS, 15);
+        DEFENSE.put(BOOTS, 13);
+        DEFENSE.put(BODY, 16);
+    }
+
+    protected static final Holder<ArmorMaterial> RENDELITHIC = Holder.direct(new ArmorMaterial("angelblock:rendelithic_armor",
             21, new int[] { 3, 6, 8, 3 }, 17, SoundEvents.ARMOR_EQUIP_DIAMOND.get(),
-            0F, 0F, () -> Ingredient.of(ItemRegistry.RENDELITHIC_INGOT.get()));
+            0F, 0F, () -> Ingredient.of(ItemRegistry.RENDELITHIC_INGOT.get())));
 
-    protected static final ArmorMaterial RENDELITHIC2 = new ModArmorMaterial("angelblock:rendelithic_armor2",
+    protected static final Holder<ArmorMaterial> RENDELITHIC2 = Holder.direct(new ModArmorMaterial("angelblock:rendelithic_armor2",
             21, new int[] { 3, 6, 8, 3 }, 17, SoundEvents.ARMOR_EQUIP_DIAMOND.get(),
-            0F, 0F, () -> Ingredient.of(ItemRegistry.RENDELITHIC_INGOT.get()));
+            0F, 0F, () -> Ingredient.of(ItemRegistry.RENDELITHIC_INGOT.get())));
 
-    protected static final ArmorMaterial LIMONITE = new ModArmorMaterial("angelblock:limonite_armor",
+    protected static final Holder<ArmorMaterial> LIMONITE = Holder.direct(new ModArmorMaterial("angelblock:limonite_armor",
             28, new int[] { 3, 5, 8, 3 }, 30, SoundEvents.ARMOR_EQUIP_LEATHER.get(),
-            1F, 0F, () -> Ingredient.of(ItemRegistry.LIMONITE_INGOT.get()));
+            1F, 0F, () -> Ingredient.of(ItemRegistry.LIMONITE_INGOT.get())));
 
-    protected static final ArmorMaterial LIMONITE2 = new ModArmorMaterial("angelblock:limonite_armor2",
+    protected static final Holder<ArmorMaterial> LIMONITE2 = Holder.direct(new ModArmorMaterial("angelblock:limonite_armor2",
             28, new int[] { 3, 5, 8, 3 }, 30, SoundEvents.ARMOR_EQUIP_LEATHER.get(),
-            1F, 0F, () -> Ingredient.of(ItemRegistry.LIMONITE_INGOT.get()));
+            1F, 0F, () -> Ingredient.of(ItemRegistry.LIMONITE_INGOT.get())));
 
-    protected static final ArmorMaterial DIAMITHIC = new ModArmorMaterial("angelblock:diamithic_armor",
+    protected static final Holder<ArmorMaterial> DIAMITHIC = Holder.direct(new ModArmorMaterial("angelblock:diamithic_armor",
             41, new int[] { 3, 6, 9, 3 }, 14, SoundEvents.ARMOR_EQUIP_DIAMOND.get(),
-            3F, 0.1F, () -> Ingredient.of(ItemRegistry.DIAMITHIC_INGOT.get()));
+            3F, 0.1F, () -> Ingredient.of(ItemRegistry.DIAMITHIC_INGOT.get())));
 
-    protected static final ArmorMaterial DIAMITHIC2 = new ModArmorMaterial("angelblock:diamithic_armor2",
+    protected static final Holder<ArmorMaterial> DIAMITHIC2 = Holder.direct(new ModArmorMaterial("angelblock:diamithic_armor2",
             41, new int[] { 3, 6, 9, 3 }, 14, SoundEvents.ARMOR_EQUIP_DIAMOND.get(),
-            3F, 0.1F, () -> Ingredient.of(ItemRegistry.DIAMITHIC_INGOT.get()));
+            3F, 0.1F, () -> Ingredient.of(ItemRegistry.DIAMITHIC_INGOT.get())));
 
-    protected static final ArmorMaterial LAPIS = new ModArmorMaterial("angelblock:lapis_armor",
+    protected static final Holder<ArmorMaterial> LAPIS = Holder.direct(new ModArmorMaterial("angelblock:lapis_armor",
             37, new int[] { 3, 6, 8, 3 }, 12, SoundEvents.ARMOR_EQUIP_DIAMOND.get(),
-            2F, 0F, () -> Ingredient.of(ItemRegistry.LAPIS_INGOT.get()));
+            2F, 0F, () -> Ingredient.of(ItemRegistry.LAPIS_INGOT.get())));
 
-    protected static final ArmorMaterial LAPIS2 = new ModArmorMaterial("angelblock:lapis_armor2",
+    protected static final Holder<ArmorMaterial> LAPIS2 = Holder.direct(new ModArmorMaterial("angelblock:lapis_armor2",
             37, new int[] { 3, 6, 8, 3 }, 12, SoundEvents.ARMOR_EQUIP_DIAMOND.get(),
-            2F, 0F, () -> Ingredient.of(ItemRegistry.LAPIS_INGOT.get()));
+            2F, 0F, () -> Ingredient.of(ItemRegistry.LAPIS_INGOT.get())));
 
-    protected static final ArmorMaterial SUPER = new ModArmorMaterial("angelblock:super_armor",
+    protected static final Holder<ArmorMaterial> SUPER = Holder.direct(new ModArmorMaterial("angelblock:super_armor",
             43, new int[] { 3, 6, 9, 3 }, 25, SoundEvents.ARMOR_EQUIP_DIAMOND.get(),
-            4F, 0.2F, () -> Ingredient.of(ItemRegistry.SUPER_INGOT.get()));
+            4F, 0.2F, () -> Ingredient.of(ItemRegistry.SUPER_INGOT.get())));
 
-    protected static final ArmorMaterial SUPER2 = new ModArmorMaterial("angelblock:super_armor2",
+    protected static final Holder<ArmorMaterial> SUPER2 = Holder.direct(new ModArmorMaterial("angelblock:super_armor2",
             43, new int[] { 3, 6, 9, 3 }, 25, SoundEvents.ARMOR_EQUIP_DIAMOND.get(),
-            4F, 0.2F, () -> Ingredient.of(ItemRegistry.SUPER_INGOT.get()));
+            4F, 0.2F, () -> Ingredient.of(ItemRegistry.SUPER_INGOT.get())));
 
-    protected static final ArmorMaterial FLYING = new ModArmorMaterial("angelblock:flying_armor",
+    protected static final Holder<ArmorMaterial> FLYING = Holder.direct(new ModArmorMaterial("angelblock:flying_armor",
             3, new int[] { 1, 1, 1, 1 }, 30, SoundEvents.ARMOR_EQUIP_ELYTRA.get(),
-            0F, 0F, () -> Ingredient.of(Items.PHANTOM_MEMBRANE));
+            0F, 0F, () -> Ingredient.of(Items.PHANTOM_MEMBRANE)));
 
     public ModArmorMaterial(String name, int durabilityMultiplier, int[] slotProtections, int enchantmentValue,
                             SoundEvent sound, float toughness, float knockbackResistance, Supplier<Ingredient> repairIngredient)
     {
         this.name = name;
-        this.durabilityMultiplier = durabilityMultiplier;
         this.slotProtections = slotProtections;
         this.enchantmentValue = enchantmentValue;
         this.sound = sound;
         this.toughness = toughness;
         this.knockbackResistance = knockbackResistance;
         this.repairIngredient = repairIngredient;
-    }
-
-    @Override
-    public int getDurabilityForType(ArmorItem.Type armorType)
-    {
-        return HEALTH_PER_SLOT[armorType.getSlot().getIndex()] * this.durabilityMultiplier;
     }
 
     @Override
