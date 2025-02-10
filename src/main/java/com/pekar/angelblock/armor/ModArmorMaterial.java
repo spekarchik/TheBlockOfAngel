6f4c9dd9db3bafbed12db6130a7a26d9a57655b3
PEKAR\Sergey <sergey.pekarchik@gmail.com>
2025-02-11 00:55:11 +0100

    armor material fix

--- src/main/java/com/pekar/angelblock/armor/ModArmorMaterial.java
+++ src/main/java/com/pekar/angelblock/armor/ModArmorMaterial.java
@@ -119,7 +119,7 @@ public class ModArmorMaterial
 
         return Registry.registerForHolder(
                 BuiltInRegistries.ARMOR_MATERIAL,
-                Utils.createResourceLocation(armorName),
+                Utils.createResourceLocation(Main.MODID, armorName),
                 new ArmorMaterial(enummap, enchantmentValue, equipSound, repairIngredient, armorLayers, toughness, knockbackResistance)
         );
     }
@@ -137,8 +137,7 @@ public class ModArmorMaterial
 
     private static List<ArmorMaterial.Layer> getNonDyeableArmorLayers(String armorName)
     {
-        var fullArmorName = getFullArmorModelName(armorName);
-        var resourceLocation = Utils.createResourceLocation(fullArmorName);
+        var resourceLocation = Utils.createResourceLocation(Main.MODID, armorName);
         return List.of(new ArmorMaterial.Layer(resourceLocation));
     }
 }
