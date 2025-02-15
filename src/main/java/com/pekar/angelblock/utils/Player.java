cf98e4b61f93a135171247a2e24f137ef98b5c08
PEKAR\Sergey <sergey.pekarchik@gmail.com>
2025-02-16 00:58:00 +0100

    enhanced drop with fortune fixed

--- src/main/java/com/pekar/angelblock/utils/Player.java
+++ src/main/java/com/pekar/angelblock/utils/Player.java
@@ -2,10 +2,19 @@ package com.pekar.angelblock.utils;
 
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.BaseFireBlock;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.gameevent.GameEvent;
+import net.minecraft.world.level.material.FluidState;
+import org.jetbrains.annotations.NotNull;
+
+import javax.annotation.Nullable;
 
 public class Player
 {
@@ -67,8 +76,40 @@ public class Player
 //                int exp = blockState.getExpDrop(level, pos, null, entityLiving, mainHandItemStack);
 //                exp = applyFortuneBonus(exp, fortuneLevel, 1.7);
 //                block.popExperience((ServerLevel) level, pos, exp);
-        level.destroyBlock(pos, true, entityLiving);
-        return true;
+        final int recursionLeft = 512;
+        return destroyBlock(pos, true, entityLiving, level, entityLiving.getMainHandItem(), recursionLeft);
     }
 
+    // See Level.destroyBlock()
+    private boolean destroyBlock(BlockPos pos, boolean dropBlock, @NotNull Entity entity, Level level, ItemStack tool, int recursionLeft)
+    {
+        BlockState blockstate = level.getBlockState(pos);
+        
+        if (blockstate.isAir())
+        {
+            return false;
+        }
+        else
+        {
+            FluidState fluidstate = level.getFluidState(pos);
+            if (!(blockstate.getBlock() instanceof BaseFireBlock))
+            {
+                level.levelEvent(2001, pos, Block.getId(blockstate));
+            }
+
+            if (dropBlock)
+            {
+                BlockEntity blockentity = blockstate.hasBlockEntity() ? level.getBlockEntity(pos) : null;
+                Block.dropResources(blockstate, level, pos, blockentity, entity, tool);
+            }
+
+            boolean flag = level.setBlock(pos, fluidstate.createLegacyBlock(), 3, recursionLeft);
+            if (flag)
+            {
+                level.gameEvent(GameEvent.BLOCK_DESTROY, pos, GameEvent.Context.of(entity, blockstate));
+            }
+
+            return flag;
+        }
+    }
 }
