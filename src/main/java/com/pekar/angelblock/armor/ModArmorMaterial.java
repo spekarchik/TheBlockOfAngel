d10db0c6685ac1e0d06f5ba1f8c92a2046f3da05
PEKAR\Sergey <sergey.pekarchik@gmail.com>
2025-02-20 23:20:50 +0100

    armor materials updated

--- src/main/java/com/pekar/angelblock/armor/ModArmorMaterial.java
+++ src/main/java/com/pekar/angelblock/armor/ModArmorMaterial.java
@@ -46,19 +46,19 @@ public class ModArmorMaterial
     public static final String FLYING_MATERIAL_NAME = "flying";
 
     protected static final ModArmorMaterial RENDELITHIC = new ModArmorMaterial(RENDELITHIC_MATERIAL_NAME, "rendelithic_armor",
-            createArmorTypeMap(3, 6, 8, 3, 7),
+            createArmorTypeMap(4, 8, 10, 4, 7),
             25, 0F, 0F, 11, SoundEvents.ARMOR_EQUIP_GOLD, () -> Ingredient.of(ItemRegistry.RENDELITHIC_INGOT.get()));
     protected static final ModArmorMaterial LIMONITE = new ModArmorMaterial(LIMONITE_MATERIAL_NAME, "limonite_armor",
             createArmorTypeMap(2, 5, 7, 3, 3),
             30, 1F, 0F, 23, SoundEvents.ARMOR_EQUIP_LEATHER, () -> Ingredient.of(ItemRegistry.LIMONITE_INGOT.get()));
     protected static final ModArmorMaterial DIAMITHIC = new ModArmorMaterial(DIAMITHIC_MATERIAL_NAME, "diamithic_armor",
-            createArmorTypeMap(3, 6, 9, 3, 11),
-            14, 3F, 0.2F, 41, SoundEvents.ARMOR_EQUIP_NETHERITE, () -> Ingredient.of(ItemRegistry.DIAMITHIC_INGOT.get()));
+            createArmorTypeMap(3, 7, 9, 3, 11),
+            14, 3F, 0.2F, 45, SoundEvents.ARMOR_EQUIP_NETHERITE, () -> Ingredient.of(ItemRegistry.DIAMITHIC_INGOT.get()));
     protected static final ModArmorMaterial LAPIS = new ModArmorMaterial(LAPIS_MATERIAL_NAME, "lapis_armor",
             createArmorTypeMap(3, 6, 8, 3, 11),
-            10, 2F, 0F,30, SoundEvents.ARMOR_EQUIP_DIAMOND, () -> Ingredient.of(ItemRegistry.LAPIS_INGOT.get()));
+            10, 0F, 0F,40, SoundEvents.ARMOR_EQUIP_DIAMOND, () -> Ingredient.of(ItemRegistry.LAPIS_INGOT.get()));
     protected static final ModArmorMaterial SUPER = new ModArmorMaterial(SUPER_MATERIAL_NAME, "super_armor",
-            createArmorTypeMap(3, 6, 9, 3, 15),
+            createArmorTypeMap(5, 9, 11, 5, 15),
             1, 4F, 0.2F, 43, SoundEvents.ARMOR_EQUIP_NETHERITE, () -> Ingredient.of(ItemRegistry.SUPER_INGOT.get()));
     protected static final ModArmorMaterial FLYING = new ModArmorMaterial(FLYING_MATERIAL_NAME, "flying_armor",
             createArmorTypeMap(1, 1, 1, 1, 1),
