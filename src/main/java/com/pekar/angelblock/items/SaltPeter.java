f151af9a36730b720b0ff2ba872243172b0a2795
PEKAR\Sergey <sergey.pekarchik@gmail.com>
2025-03-11 01:35:51 +0100

    description improved

---
+++ src/main/java/com/pekar/angelblock/items/SaltPeter.java
@@ -0,0 +1,24 @@
+package com.pekar.angelblock.items;
+
+import com.pekar.angelblock.TextStyle;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
+
+import java.util.List;
+
+public class SaltPeter extends ModItemWithHoverText
+{
+    public SaltPeter()
+    {
+        super(TextStyle.Notice);
+    }
+
+    @Override
+    public void appendHoverText(ItemStack stack, TooltipContext context, List<Component> components, TooltipFlag tooltipFlag)
+    {
+        if (!utils.text.showExtendedDescription(components)) return;
+
+        super.appendHoverText(stack, context, components, tooltipFlag);
+    }
+}
