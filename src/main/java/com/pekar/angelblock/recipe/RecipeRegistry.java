2a4fc967da6327fb5fa1cf029ebc624a523ea042
PEKAR\Sergey <sergey.pekarchik@gmail.com>
2025-03-12 17:45:28 +0100

    Potion recipes improvement

--- src/main/java/com/pekar/angelblock/recipe/RecipeRegistry.java
+++ src/main/java/com/pekar/angelblock/recipe/RecipeRegistry.java
@@ -35,24 +35,22 @@ public class RecipeRegistry
 
     private static void registerBlockBreakerBrewing(PotionBrewing.Builder builder)
     {
-        var ingredient = new Ingredient(new AwkwardSplashPotionIngredient());
         //var input = new ItemStack(Items.SPLASH_POTION);
         var output = new ItemStack(ItemRegistry.BLOCK_BREAKER_POTION_ITEM.get());
         //input.set(DataComponents.POTION_CONTENTS, new PotionContents(Potions.AWKWARD)); // see PotionItem.getDefaultInstance()
 //        output.set(DataComponents.POTION_CONTENTS, new PotionContents(PotionRegistry.BLOCK_BREAKER_POTION));
-        var recipe = new BrewingRecipe(ingredient, Ingredient.of(Items.PRISMARINE_SHARD), output);
+        var recipe = new BrewingRecipe(AWKWARD_SPLASH_POTION, Ingredient.of(Items.PRISMARINE_SHARD), output);
 
         builder.addRecipe(recipe);
     }
 
     private static void registerSoaringSporeEssence(PotionBrewing.Builder builder)
     {
-        var ingredient = new Ingredient(new SlowFallingPotionIngredient());
         //var input = new ItemStack(Items.POTION);
         //input.set(DataComponents.POTION_CONTENTS, new PotionContents(Potions.SLOW_FALLING));
         var output = new ItemStack(ItemRegistry.SOARING_SPORE_ESSENCE.get());
         TagKey<Item> froglightsTag = TagKey.create(Registries.ITEM, ResourceLocation.fromNamespaceAndPath("minecraft", "froglights"));
-        var recipe = new BrewingRecipe(ingredient, Ingredient.of(froglightsTag), output);
+        var recipe = new BrewingRecipe(SLOW_FALLING_POTION, Ingredient.of(froglightsTag), output);
 
         builder.addRecipe(recipe);
     }
