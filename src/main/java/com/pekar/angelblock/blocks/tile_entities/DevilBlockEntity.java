9f93d7c738d3ab8d3c54349628ce7e4f9095cd03
PEKAR\Sergey <sergey.pekarchik@gmail.com>
2025-03-20 00:36:55 +0100

    Devil Block fix

--- src/main/java/com/pekar/angelblock/blocks/tile_entities/DevilBlockEntity.java
+++ src/main/java/com/pekar/angelblock/blocks/tile_entities/DevilBlockEntity.java
@@ -142,7 +142,7 @@ public class DevilBlockEntity extends BlockEntity implements ILivingDeathEventHa
 
         if (distance < SQR_EFFECTIVE_RADIUS)
         {
-            //entity.setHealth(entity.getMaxHealth());
+            entity.setHealth(1.0F);
             event.setCanceled(true);
         }
     }
