7cc413b5fa8526f24e11787bce458886ae07ccca
PEKAR\Sergey <sergey.pekarchik@gmail.com>
2025-03-23 22:09:50 +0100

    Nether Bars block improved

--- src/main/java/com/pekar/angelblock/blocks/NetherBarsBlock.java
+++ src/main/java/com/pekar/angelblock/blocks/NetherBarsBlock.java
@@ -10,13 +10,14 @@ import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.properties.BlockStateProperties;
-import net.minecraft.world.level.block.state.properties.EnumProperty;
+import net.minecraft.world.level.block.state.properties.BooleanProperty;
 import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.Shapes;
 import net.minecraft.world.phys.shapes.VoxelShape;
@@ -27,41 +28,146 @@ import java.util.List;
 
 public class NetherBarsBlock extends ModBlockWithDoubleHoverText
 {
-    private static final VoxelShape HITBOX_Z = Shapes.box(
+    private static final VoxelShape HITBOX_WEST_EAST = Shapes.box(
             0.0 / 16.0, 0.0 / 16.0, 7.0 / 16.0,
             16.0 / 16.0, 16.0 / 16.0, 9.0 / 16.0
     );
 
-    private static final VoxelShape HITBOX_X = Shapes.box(
+    private static final VoxelShape HITBOX_NORTH_SOUTH = Shapes.box(
             7.0 / 16.0, 0.0 / 16.0, 0.0 / 16.0,
             9.0 / 16.0, 16.0 / 16.0, 16.0 / 16.0
     );
 
-    public static final EnumProperty<Direction.Axis> HORIZONTAL_AXIS = BlockStateProperties.HORIZONTAL_AXIS;
+    private static final VoxelShape HITBOX_COLUMN = Shapes.box(
+            7.0 / 16.0, 0.0 / 16.0, 7.0 / 16.0,
+            9.0 / 16.0, 16.0 / 16.0, 9.0 / 16.0
+    );
+
+    private static final VoxelShape HITBOX_CORNER_EAST_NORTH = Shapes.or(
+            Shapes.box(7.0 / 16.0, 0.0 / 16.0, 7.0 / 16.0, 16.0 / 16.0, 16.0 / 16.0, 9.0 / 16.0),
+            Shapes.box(7.0 / 16.0, 0.0 / 16.0, 0.0 / 16.0, 9.0 / 16.0, 16.0 / 16.0, 9.0 / 16.0)
+    );
+
+    private static final VoxelShape HITBOX_CORNER_EAST_SOUTH = Shapes.or(
+            Shapes.box(7.0 / 16.0, 0.0 / 16.0, 7.0 / 16.0, 9.0 / 16.0, 16.0 / 16.0, 16.0 / 16.0),
+            Shapes.box(7.0 / 16.0, 0.0 / 16.0, 7.0 / 16.0, 16.0 / 16.0, 16.0 / 16.0, 9.0 / 16.0)
+    );
+
+    private static final VoxelShape HITBOX_CORNER_WEST_NORTH = Shapes.or(
+            Shapes.box(7.0 / 16.0, 0.0 / 16.0, 0.0 / 16.0, 9.0 / 16.0, 16.0 / 16.0, 9.0 / 16.0),
+            Shapes.box(0.0 / 16.0, 0.0 / 16.0, 7.0 / 16.0, 9.0 / 16.0, 16.0 / 16.0, 9.0 / 16.0)
+    );
+
+    private static final VoxelShape HITBOX_CORNER_WEST_SOUTH = Shapes.or(
+            Shapes.box(0.0 / 16.0, 0.0 / 16.0, 7.0 / 16.0, 9.0 / 16.0, 16.0 / 16.0, 9.0 / 16.0),
+            Shapes.box(7.0 / 16.0, 0.0 / 16.0, 7.0 / 16.0, 9.0 / 16.0, 16.0 / 16.0, 16.0 / 16.0)
+    );
+
+    private static final VoxelShape HITBOX_CROSS = Shapes.or(
+            Shapes.box(0.0 / 16.0, 0.0 / 16.0, 7.0 / 16.0, 16.0 / 16.0, 16.0 / 16.0, 9.0 / 16.0),
+            Shapes.box(7.0 / 16.0, 0.0 / 16.0, 0.0 / 16.0, 9.0 / 16.0, 16.0 / 16.0, 16.0 / 16.0)
+    );
+
+    private static final VoxelShape HITBOX_T_EAST = Shapes.or(
+            Shapes.box(7.0 / 16.0, 0.0 / 16.0, 7.0 / 16.0, 16.0 / 16.0, 16.0 / 16.0, 9.0 / 16.0),
+            Shapes.box(7.0 / 16.0, 0.0 / 16.0, 0.0 / 16.0, 9.0 / 16.0, 16.0 / 16.0, 16.0 / 16.0)
+    );
+
+    private static final VoxelShape HITBOX_T_WEST = Shapes.or(
+            Shapes.box(0.0 / 16.0, 0.0 / 16.0, 7.0 / 16.0, 9.0 / 16.0, 16.0 / 16.0, 9.0 / 16.0),
+            Shapes.box(7.0 / 16.0, 0.0 / 16.0, 0.0 / 16.0, 9.0 / 16.0, 16.0 / 16.0, 16.0 / 16.0)
+    );
+
+    private static final VoxelShape HITBOX_T_SOUTH = Shapes.or(
+            Shapes.box(0.0 / 16.0, 0.0 / 16.0, 7.0 / 16.0, 16.0 / 16.0, 16.0 / 16.0, 9.0 / 16.0),
+            Shapes.box(7.0 / 16.0, 0.0 / 16.0, 7.0 / 16.0, 9.0 / 16.0, 16.0 / 16.0, 16.0 / 16.0)
+    );
+
+    private static final VoxelShape HITBOX_T_NORTH = Shapes.or(
+            Shapes.box(0.0 / 16.0, 0.0 / 16.0, 7.0 / 16.0, 16.0 / 16.0, 16.0 / 16.0, 9.0 / 16.0),
+            Shapes.box(7.0 / 16.0, 0.0 / 16.0, 0.0 / 16.0, 9.0 / 16.0, 16.0 / 16.0, 9.0 / 16.0)
+    );
+
+    public static final BooleanProperty NORTH = BlockStateProperties.NORTH;
+    public static final BooleanProperty SOUTH = BlockStateProperties.SOUTH;
+    public static final BooleanProperty WEST = BlockStateProperties.WEST;
+    public static final BooleanProperty EAST = BlockStateProperties.EAST;
 
     public NetherBarsBlock()
     {
         super(BlockBehaviour.Properties.ofFullCopy(Blocks.IRON_BARS).strength(10F, 1200F).requiresCorrectToolForDrops());
-        this.registerDefaultState(this.stateDefinition.any().setValue(HORIZONTAL_AXIS, Direction.Axis.X));
+        this.registerDefaultState(this.stateDefinition.any()
+                .setValue(NORTH, false)
+                .setValue(SOUTH, false)
+                .setValue(WEST, false)
+                .setValue(EAST, false)
+        );
     }
 
     @Override
     protected @NotNull VoxelShape getShape(BlockState state, BlockGetter level, BlockPos pos, CollisionContext context)
     {
-        return (state.getValue(HORIZONTAL_AXIS) == Direction.Axis.X) ? HITBOX_X : HITBOX_Z;
+        boolean north = state.getValue(NORTH);
+        boolean south = state.getValue(SOUTH);
+        boolean west = state.getValue(WEST);
+        boolean east = state.getValue(EAST);
+
+        if (north && south && west && east) return HITBOX_CROSS;
+        if (north && south && west) return HITBOX_T_WEST;
+        if (north && south && east) return HITBOX_T_EAST;
+        if (west && east && north) return HITBOX_T_NORTH;
+        if (west && east && south) return HITBOX_T_SOUTH;
+        if (west && north) return HITBOX_CORNER_WEST_NORTH;
+        if (west && south) return HITBOX_CORNER_WEST_SOUTH;
+        if (east && north) return HITBOX_CORNER_EAST_NORTH;
+        if (east && south) return HITBOX_CORNER_EAST_SOUTH;
+        if (north || south) return HITBOX_NORTH_SOUTH;
+        if (west || east) return HITBOX_WEST_EAST;
+
+        return HITBOX_COLUMN;
     }
 
     @Override
     protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder)
     {
-        builder.add(HORIZONTAL_AXIS);
+        builder.add(NORTH, SOUTH, WEST, EAST);
     }
 
     @Override
     public @Nullable BlockState getStateForPlacement(BlockPlaceContext context)
     {
-        var axis = context.getHorizontalDirection().getAxis();
-        return defaultBlockState().setValue(HORIZONTAL_AXIS, axis);
+        var level = context.getLevel();
+        var pos = context.getClickedPos();
+
+        boolean north = connectsTo(level.getBlockState(pos.north()), level, pos);
+        boolean south = connectsTo(level.getBlockState(pos.south()), level, pos);
+        boolean west = connectsTo(level.getBlockState(pos.west()), level, pos);
+        boolean east = connectsTo(level.getBlockState(pos.east()), level, pos);
+
+        return this.defaultBlockState()
+                .setValue(NORTH, north)
+                .setValue(SOUTH, south)
+                .setValue(WEST, west)
+                .setValue(EAST, east);
+    }
+
+    @Override
+    public BlockState updateShape(BlockState state, Direction direction, BlockState neighborState, LevelAccessor level, BlockPos pos, BlockPos neighborPos)
+    {
+        boolean north = connectsTo(level.getBlockState(pos.north()), level, pos);
+        boolean south = connectsTo(level.getBlockState(pos.south()), level, pos);
+        boolean west = connectsTo(level.getBlockState(pos.west()), level, pos);
+        boolean east = connectsTo(level.getBlockState(pos.east()), level, pos);
+
+        return state.setValue(NORTH, north)
+                .setValue(SOUTH, south)
+                .setValue(WEST, west)
+                .setValue(EAST, east);
+    }
+
+    private boolean connectsTo(BlockState state, LevelAccessor level, BlockPos pos)
+    {
+        return (state.getBlock() instanceof NetherBarsBlock) || (state.isSolidRender(level, pos));
     }
 
     @Override
