376d31b77a9551474d713bbdb1afe4080e6b9534
PEKAR\Sergey <sergey.pekarchik@gmail.com>
2025-04-21 21:00:35 +0200

    code improvements

--- src/main/java/com/pekar/angelblock/events/armor/SuperArmor.java
+++ src/main/java/com/pekar/angelblock/events/armor/SuperArmor.java
@@ -171,7 +171,7 @@ public class SuperArmor extends Armor
                 event.setCanceled(realDamage <= 0);
             }
 
-            if (player.isFullArmorSetPutOn(this))
+            if (isFullArmorSet)
                 event.getEntity().clearFire();
         }
         else if (isHotFloorDamage(damageSource) || isFreezeDamage(damageSource))
@@ -238,7 +238,7 @@ public class SuperArmor extends Armor
         }
         else if (isLavaDamage(damageSource))
         {
-            if (player.isFullArmorSetPutOn(this))
+            if (isFullArmorSet)
             {
                 event.setNewDamage(0.05f * event.getNewDamage());
             }
