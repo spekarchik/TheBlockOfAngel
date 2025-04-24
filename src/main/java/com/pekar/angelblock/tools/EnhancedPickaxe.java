0dcefb4658014e6e107e98f11746077fa8fc05d2
PEKAR\Sergey <sergey.pekarchik@gmail.com>
2025-04-24 20:14:41 +0200

    [Cherry-Pick] Green Diamond Ore Block: gorup mining fixed

--- src/main/java/com/pekar/angelblock/tools/EnhancedPickaxe.java
+++ src/main/java/com/pekar/angelblock/tools/EnhancedPickaxe.java
@@ -1,12 +1,15 @@
 package com.pekar.angelblock.tools;
 
+import com.pekar.angelblock.blocks.BlockRegistry;
 import com.pekar.angelblock.potions.PotionRegistry;
 import com.pekar.angelblock.tools.properties.IMaterialProperties;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
+import net.minecraft.tags.BlockTags;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.RedStoneOreBlock;
 import net.minecraft.world.level.block.state.BlockState;
 
@@ -39,8 +42,7 @@ public class EnhancedPickaxe extends ModPickaxe
             return;
 
         BlockState blockState = level.getBlockState(pos);
-        var block = blockState.getBlock();
-        if (blockState.hasBlockEntity() || (blockState != block.defaultBlockState() && !(block instanceof RedStoneOreBlock))) return;
+        if (!canBeMinedInGroup(blockState)) return;
 
         float originHardness = blockState.getBlock().defaultDestroyTime();
         if (originHardness == 0.0F) return;
@@ -72,12 +74,17 @@ public class EnhancedPickaxe extends ModPickaxe
                 }
     }
 
+    private static boolean canBeMinedInGroup(BlockState blockState)
+    {
+        return !blockState.hasBlockEntity() && (blockState == blockState.getBlock().defaultBlockState() || blockState.is(BlockTags.REDSTONE_ORES) || blockState.is(BlockRegistry.GREEN_DIAMOND_ORE));
+    }
+
     protected void onBlockMining(Level level, BlockState originBlockState, float originHardness, BlockPos pos, LivingEntity entityLiving)
     {
         var blockState = level.getBlockState(pos);
 
         var block = blockState.getBlock();
-        if (blockState.hasBlockEntity() || (blockState != block.defaultBlockState() && !(block instanceof RedStoneOreBlock))) return;
+        if (!canBeMinedInGroup(blockState)) return;
 
         float hardness = block.defaultDestroyTime();
 
