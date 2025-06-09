1152991b4809bed807d2f19043bd06cac5fbad26
PEKAR\Sergey <sergey.pekarchik@gmail.com>
2025-06-10 01:00:38 +0200

    Bug fixed: Aerite armor can't be enchanted via Enchanting Table

--- src/main/java/com/pekar/angelblock/armor/FlyingArmor.java
+++ src/main/java/com/pekar/angelblock/armor/FlyingArmor.java
@@ -1,8 +1,10 @@
 package com.pekar.angelblock.armor;
 
+import net.minecraft.core.Holder;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.enchantment.Enchantment;
 import net.minecraft.world.item.equipment.ArmorType;
 
 public class FlyingArmor extends ModArmor
@@ -23,7 +25,12 @@ public class FlyingArmor extends ModArmor
     {
         return getModelName(wearer, EquipmentSlot.FEET)
             .equals(ArmorRegistry.FLYING_BOOTS.get().getArmorFamilyName());
+    }
 
+    @Override
+    public boolean supportsEnchantment(ItemStack stack, Holder<Enchantment> enchantment)
+    {
+        return false;
     }
 
     private String getModelName(LivingEntity entity, EquipmentSlot slot)
