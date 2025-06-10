f7b4edcfad52293e024fb7fb1e2e30c682009a0b
PEKAR\Sergey <sergey.pekarchik@gmail.com>
2025-06-10 16:35:14 +0200

    Planter, Builder, Track Layer: tooltip refactoring

--- src/main/java/com/pekar/angelblock/tools/WorkRod.java
+++ src/main/java/com/pekar/angelblock/tools/WorkRod.java
@@ -1,9 +1,22 @@
 package com.pekar.angelblock.tools;
 
-public class WorkRod extends ModRod
+import com.pekar.angelblock.tooltip.ITooltipProvider;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
+
+import java.util.List;
+
+public abstract class WorkRod extends ModRod implements ITooltipProvider
 {
     public WorkRod(ModToolMaterial material, Properties properties)
     {
         super(material, false, properties);
+    }
+
+    @Override
+    public void appendHoverText(ItemStack stack, TooltipContext context, List<Component> tooltipComponents, TooltipFlag tooltipFlag)
+    {
+        ITooltipProvider.appendHoverText(this, stack, context, tooltipComponents, tooltipFlag);
     }
 }
