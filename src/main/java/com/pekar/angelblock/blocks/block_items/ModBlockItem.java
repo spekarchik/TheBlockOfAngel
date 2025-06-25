17bb430c78351d0f8736b8c709b44a9fb7796d35
PEKAR\Sergey <sergey.pekarchik@gmail.com>
2025-06-25 20:28:56 +0200

    The Block of Angel: Separated The Dormant Block of Angel as an inactive version of the block

--- src/main/java/com/pekar/angelblock/blocks/block_items/ModBlockItem.java
+++ src/main/java/com/pekar/angelblock/blocks/block_items/ModBlockItem.java
@@ -9,4 +9,9 @@ public class ModBlockItem extends BlockItem
     {
         super(block, properties);
     }
+
+    protected String getItemDescriptionId()
+    {
+        return getDescriptionId();
+    }
 }
