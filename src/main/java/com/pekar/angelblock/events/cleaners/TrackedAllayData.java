c9c64a4233ebb1485ae8346d88da9f6bcf4795a1
PEKAR\Sergey <sergey.pekarchik@gmail.com>
2025-06-28 20:11:34 +0200

    Saving persistent allays to track later: final implementation

--- src/main/java/com/pekar/angelblock/events/cleaners/TrackedAllayData.java
+++ src/main/java/com/pekar/angelblock/events/cleaners/TrackedAllayData.java
@@ -2,6 +2,6 @@ package com.pekar.angelblock.events.cleaners;
 
 import java.util.UUID;
 
-public record TrackedAllayData(UUID allayUuid, UUID ownerUuid)
+record TrackedAllayData(UUID allayUuid, UUID ownerUuid)
 {
 }
