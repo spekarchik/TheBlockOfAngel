0206c104225e9d0d9c19bd0cb8a78cf7706a0558
PEKAR\Sergey <sergey.pekarchik@gmail.com>
2025-06-29 02:03:05 +0200

    code correction

--- src/main/java/com/pekar/angelblock/events/cleaners/TrackedAllayData.java
+++ src/main/java/com/pekar/angelblock/events/cleaners/TrackedAllayData.java
@@ -2,6 +2,6 @@ package com.pekar.angelblock.events.cleaners;
 
 import java.util.UUID;
 
-record TrackedAllayData(UUID allayUuid, UUID ownerUuid)
+public record TrackedAllayData(UUID allayUuid, UUID ownerUuid)
 {
 }
