3780d9059dd3ab75b0ad4c4dd4648a59ee8ad07b
PEKAR\Sergey <sergey.pekarchik@gmail.com>
2025-07-04 22:44:23 +0200

    Added a cooldown mechanic to the **Bios Diamond** and **End Sapphire** items to prevent immediate reuse of their effects

--- src/main/java/com/pekar/angelblock/items/BiosDiamond.java
+++ src/main/java/com/pekar/angelblock/items/BiosDiamond.java
@@ -1,6 +1,7 @@
 package com.pekar.angelblock.items;
 
 import com.pekar.angelblock.network.packets.PlaySoundPacket;
+import com.pekar.angelblock.potions.PotionRegistry;
 import com.pekar.angelblock.tooltip.ITooltip;
 import com.pekar.angelblock.tooltip.ITooltipProvider;
 import com.pekar.angelblock.tooltip.TextStyle;
@@ -22,6 +23,8 @@ import java.util.function.Consumer;
 
 public class BiosDiamond extends ModItem implements ITooltipProvider
 {
+    private static final int COOLDOWN_TIME = 100;
+
     public BiosDiamond(Properties properties)
     {
         super(properties);
@@ -30,13 +33,14 @@ public class BiosDiamond extends ModItem implements ITooltipProvider
     @Override
     public InteractionResult use(Level level, Player player, InteractionHand interactionHand)
     {
-        if (!player.hasEffect(MobEffects.ABSORPTION))
+        if (!player.hasEffect(MobEffects.ABSORPTION) && !player.hasEffect(PotionRegistry.BIOS_DIAMOND_COOLDOWN_EFFECT))
         {
             if (player instanceof ServerPlayer serverPlayer)
             {
                 int duration = level.getRandom().nextIntBetweenInclusive(40, 200);
                 int effectLevel = level.getRandom().nextIntBetweenInclusive(0, 4);
                 player.addEffect(new MobEffectInstance(MobEffects.ABSORPTION, duration, effectLevel, true, true));
+                player.addEffect(new MobEffectInstance(PotionRegistry.BIOS_DIAMOND_COOLDOWN_EFFECT, duration + COOLDOWN_TIME, 0, true, false, true));
                 new PlaySoundPacket(SoundEvents.LEVER_CLICK, 2.0F).sendToPlayer(serverPlayer);
             }
 
