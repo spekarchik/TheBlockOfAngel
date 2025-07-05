66b6f1cb19194df2c322a0638bf2ab696cc3f3f0
PEKAR\Sergey <sergey.pekarchik@gmail.com>
2025-07-05 16:39:57 +0200

    Cherry-pick correction

--- src/main/java/com/pekar/angelblock/tools/TrackLayer.java
+++ src/main/java/com/pekar/angelblock/tools/TrackLayer.java
@@ -14,6 +14,7 @@ import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Tier;
 import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.item.context.UseOnContext;
 import net.minecraft.world.level.Level;
