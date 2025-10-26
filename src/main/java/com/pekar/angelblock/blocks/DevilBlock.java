880ff14c43740edfae3b3d30ae783b8c7b062480
PEKAR\Sergey <sergey.pekarchik@gmail.com>
2025-10-26 02:39:31 +0200

    bug fixed: monsters remained undead if a Block of Devil had been destroyed by not a player or explosion.
    (cherry picked from commit c666a72b77fb8cf44a5cc21df6683a2f50921494)
    (cherry picked from commit ea21cf99d42a748d7840bf938d498e28095e7965)
    (cherry picked from commit ad782069176eb1a6e9ba09cce5d7fae3689e92ac)
    (cherry picked from commit 4cac40fa3bb4034d1bdbad27fa730c77196a5d0f)
    (cherry picked from commit 98734efa587a119562c90ab7715ae00515351b20)
    (cherry picked from commit c27d3d79370fb838a030269617ada3ec46bc5cdf)
    (cherry picked from commit a5908928b24d46dd22ad1f209f3e6a5a3f514965)

--- src/main/java/com/pekar/angelblock/blocks/DevilBlock.java
+++ src/main/java/com/pekar/angelblock/blocks/DevilBlock.java
@@ -11,6 +11,7 @@ import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Explosion;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.level.block.EntityBlock;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.entity.BlockEntityTicker;
@@ -61,9 +62,11 @@ public class DevilBlock extends ModBlock implements EntityBlock
         super.onBlockExploded(state, world, pos, explosion);
     }
 
-    private void disposeBlockEntity(Level world, BlockPos pos)
+    private void disposeBlockEntity(LevelAccessor level, BlockPos pos)
     {
-        var blockEntity = world.getBlockEntity(pos);
+        if (level.isClientSide()) return;
+
+        var blockEntity = level.getBlockEntity(pos);
         if (blockEntity instanceof DevilBlockEntity)
         {
             var devilBlockEntity = (DevilBlockEntity) blockEntity;
@@ -72,10 +75,17 @@ public class DevilBlock extends ModBlock implements EntityBlock
     }
 
     @Override
-    public boolean onDestroyedByPlayer(BlockState state, Level world, BlockPos pos, Player player, boolean willHarvest, FluidState fluid)
+    public void destroy(LevelAccessor level, BlockPos pos, BlockState state)
     {
-        disposeBlockEntity(world, pos);
-        return super.onDestroyedByPlayer(state, world, pos, player, willHarvest, fluid);
+        disposeBlockEntity(level, pos);
+        super.destroy(level, pos, state);
+    }
+
+    @Override
+    public boolean onDestroyedByPlayer(BlockState state, Level level, BlockPos pos, Player player, boolean willHarvest, FluidState fluid)
+    {
+        disposeBlockEntity(level, pos);
+        return super.onDestroyedByPlayer(state, level, pos, player, willHarvest, fluid);
     }
 
     @Override
