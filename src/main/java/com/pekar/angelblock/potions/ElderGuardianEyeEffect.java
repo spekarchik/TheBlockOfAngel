4d95e0f14d94bf269c0c2b77115ce5a01473d1e6
PEKAR\Sergey <sergey.pekarchik@gmail.com>
2026-01-14 02:38:19 +0100

    Resolved a conflict between Night Vision effects applied by the Elder Guardian Eye and helmets. The effect applied by the Elder Guardian Eye now has its own icon.
    (cherry picked from commit 1085e06447824dcf2e1a91f511471f5027a52ffb)
    (cherry picked from commit bdc4b953de3ede5726228b801e99c4d1862256a5)
    (cherry picked from commit 9769b6cbc82a20048fd06a112cccee537f1e565a)
    (cherry picked from commit 88cc31e4173920cdcef0332be7a451ee8ca2ddb1)
    (cherry picked from commit 45f3f8ad774ecdce58ce25df73689abc243c78e1)
    (cherry picked from commit 81f0c5e7de6da0710e13ba4cfb85921fb06d9c1b)

---
+++ src/main/java/com/pekar/angelblock/potions/ElderGuardianEyeEffect.java
@@ -0,0 +1,48 @@
+package com.pekar.angelblock.potions;
+
+import com.pekar.angelblock.events.player.ModMobEffectInstance;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.effect.MobEffect;
+import net.minecraft.world.effect.MobEffectCategory;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
+import net.minecraft.world.entity.LivingEntity;
+
+public class ElderGuardianEyeEffect extends MobEffect
+{
+    protected ElderGuardianEyeEffect()
+    {
+        super(MobEffectCategory.NEUTRAL, 0);
+    }
+
+    @Override
+    public boolean isInstantenous()
+    {
+        return false;
+    }
+
+    @Override
+    public boolean shouldApplyEffectTickThisTick(int duration, int amplifier)
+    {
+        return duration > 0 || duration == MobEffectInstance.INFINITE_DURATION;
+    }
+
+    @Override
+    public boolean applyEffectTick(ServerLevel level, LivingEntity entity, int amplifier)
+    {
+        if (entity.hasEffect(MobEffects.NIGHT_VISION)) return true;
+
+        var nightVisionEffectInstance = new ModMobEffectInstance(MobEffects.NIGHT_VISION, MobEffectInstance.INFINITE_DURATION, 0, false, false, false, true);
+        entity.addEffect(nightVisionEffectInstance, entity);
+
+        return true;
+    }
+
+    public void removeEffectFor(LivingEntity entity)
+    {
+        if (entity.hasEffect(MobEffects.NIGHT_VISION))
+        {
+            entity.removeEffect(MobEffects.NIGHT_VISION);
+        }
+    }
+}
