91b072287eb4779917c34affc2eda03b2d5eb2c0
PEKAR\Sergey <sergey.pekarchik@gmail.com>
2026-01-14 18:02:34 +0100

    EnergyCrystal effect improvements: it now restores after Heavy Jump has disactivated
    (cherry picked from commit 5e6ed4a95b5d8820bab8b91cba7656550c3a2208)

--- src/main/java/com/pekar/angelblock/events/effect/TemporaryPersistentArmorEffect.java
+++ src/main/java/com/pekar/angelblock/events/effect/TemporaryPersistentArmorEffect.java
@@ -60,5 +60,6 @@ class TemporaryPersistentArmorEffect extends TemporaryBaseArmorEffect<ITemporary
     {
         setState(State.OFF);
         clearEffectInstance();
+        onDeactivated();
     }
 }
