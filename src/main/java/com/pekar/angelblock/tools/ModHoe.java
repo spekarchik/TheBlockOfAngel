7a17925624f9eeeb1ba674d277b15a755677f1e7
PEKAR\Sergey <sergey.pekarchik@gmail.com>
2026-01-20 20:24:30 +0100

    Rods updated: they now drop blocks by farming instead of use action. Rods sound fixed and improvements. Removed unnecessary Jump icon when Aeryte Armor's Super Jump effect is active. In Enhanced Mode, pickaxes now treat regular and deepslate variants of the same ore as the same ore.
    (cherry picked from commit 759ac513c93e30281ce60d88fabd74d04a7b53c5)
    (cherry picked from commit 48624b6522172ae10cd228decdf1cb49a6ac6f48)

--- src/main/java/com/pekar/angelblock/tools/ModHoe.java
+++ src/main/java/com/pekar/angelblock/tools/ModHoe.java
@@ -15,7 +15,6 @@ import net.minecraft.sounds.SoundSource;
 import net.minecraft.tags.BlockTags;
 import net.minecraft.util.Mth;
 import net.minecraft.world.InteractionResult;
-import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.TooltipFlag;
@@ -119,7 +118,7 @@ public class ModHoe extends ModTool implements IModToolEnhanceable
         {
             if (!level.isClientSide)
             {
-                setBlock(player, pos, Blocks.DIRT);
+                setBlockWithClientSound(player, pos, Blocks.DIRT);
                 damageMainHandItemIfSurvivalIgnoreClient(player, level);
             }
 
