23d454de413966f97f4130aab53bbedde0bb4796
PEKAR\Sergey <sergey.pekarchik@gmail.com>
2026-01-23 20:25:55 +0100

    **Builder**, **Planter**, and **Track Layer** recipes now only work if the source rod is undamaged.
    (cherry picked from commit c75250f6d535a1ffd7ec1f7d6de83b5bc03adf4b)
    (cherry picked from commit 1360807b0f128025073df4276bccd6b700f39141)
    (cherry picked from commit 3065fc48701b5405c67c47472472f787413091c4)
    (cherry picked from commit 97b1923b40583132ecdd410eb83d9acb6caea44f)

---
+++ src/main/java/com/pekar/angelblock/menus/CustomCraftingMenu.java
@@ -0,0 +1,65 @@
+package com.pekar.angelblock.menus;
+
+import com.pekar.angelblock.tools.ToolRegistry;
+import net.minecraft.world.Container;
+import net.minecraft.world.entity.player.Inventory;
+import net.minecraft.world.inventory.ContainerLevelAccess;
+import net.minecraft.world.inventory.CraftingMenu;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+
+public class CustomCraftingMenu extends CraftingMenu
+{
+    public CustomCraftingMenu(int containerId, Inventory playerInventory)
+    {
+        super(containerId, playerInventory);
+    }
+
+    public CustomCraftingMenu(int containerId, Inventory playerInventory, ContainerLevelAccess access)
+    {
+        super(containerId, playerInventory, access);
+    }
+
+    @Override
+    public void slotsChanged(Container inventory)
+    {
+        super.slotsChanged(inventory);
+
+        var result = resultSlots.getItem(0);
+
+        if (!result.isEmpty() && result.is(ToolRegistry.BUILDER.get()))
+        {
+            if (hasDamagedRod(ToolRegistry.AMETHYST_ROD.get()))
+            {
+                resultSlots.setItem(0, ItemStack.EMPTY);
+            }
+        }
+        else if (!result.isEmpty() && result.is(ToolRegistry.TRACK_LAYER.get()))
+        {
+            if (hasDamagedRod(ToolRegistry.MARINE_ROD.get()))
+            {
+                resultSlots.setItem(0, ItemStack.EMPTY);
+            }
+        }
+        else if (!result.isEmpty() && result.is(ToolRegistry.PLANTER.get()))
+        {
+            if (hasDamagedRod(ToolRegistry.ANCIENT_ROD.get()))
+            {
+                resultSlots.setItem(0, ItemStack.EMPTY);
+            }
+        }
+    }
+
+    private boolean hasDamagedRod(Item rodItem)
+    {
+        for (int i = 0; i < this.craftSlots.getContainerSize(); i++)
+        {
+            var stack = this.craftSlots.getItem(i);
+            if (stack.is(rodItem) && stack.isDamaged())
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+}
