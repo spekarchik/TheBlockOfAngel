6258cf1212269931f2e396cd46f4e6e827d947d7
PEKAR\Sergey <sergey.pekarchik@gmail.com>
2026-02-20 21:52:33 +0100

    Interactions using Marine Crystal and Soaring Spore Essence have been improved. Belarusian localization improved
    (cherry picked from commit 23b21b193c76a208a52095159b82aef676c74cd1)

--- src/main/java/com/pekar/angelblock/items/MarineCrystal.java
+++ src/main/java/com/pekar/angelblock/items/MarineCrystal.java
@@ -4,13 +4,6 @@ import com.pekar.angelblock.tooltip.ITooltip;
 import com.pekar.angelblock.tooltip.ITooltipProvider;
 import com.pekar.angelblock.tooltip.TextStyle;
 import net.minecraft.network.chat.Component;
-import net.minecraft.server.level.ServerPlayer;
-import net.minecraft.sounds.SoundEvents;
-import net.minecraft.sounds.SoundSource;
-import net.minecraft.world.InteractionHand;
-import net.minecraft.world.InteractionResult;
-import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.item.component.TooltipDisplay;
@@ -22,27 +15,6 @@ public class MarineCrystal extends ModItem implements ITooltipProvider
     public MarineCrystal(Properties properties)
     {
         super(properties);
-    }
-
-    @Override
-    public InteractionResult interactLivingEntity(ItemStack stack, Player player, LivingEntity entity, InteractionHand usedHand)
-    {
-        var level = player.level();
-        var isClientSide = level.isClientSide();
-
-        if (entity instanceof Player) return InteractionResult.FAIL;
-
-        if (usedHand != InteractionHand.MAIN_HAND)
-            return InteractionResult.PASS;
-
-        if (player instanceof ServerPlayer serverPlayer)
-        {
-            boolean result = entity.removeAllEffects();
-            if (result)
-                level.playSound(null, entity, SoundEvents.ENCHANTMENT_TABLE_USE, SoundSource.NEUTRAL, 1F, 1F);
-        }
-
-        return sidedSuccess(isClientSide);
     }
 
     @Override
