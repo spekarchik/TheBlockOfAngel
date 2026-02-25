b661143c38e639ef7c2a14800a0fc6db18cda808
PEKAR\Sergey <sergey.pekarchik@gmail.com>
2026-02-25 03:49:15 +0100

    Added animal armor: Wolf Rendelite Armor and Horse Lymonite Armor. Added rare handbooks to craft the animal armor. Chance to find a handbook in Stronghold Library has been decreased. Lymonite Horse Armor: freeze animation removed. Horse unfreezing process improved.

---
+++ src/main/java/com/pekar/angelblock/armor/HorseLimoniteArmor.java
@@ -0,0 +1,19 @@
+package com.pekar.angelblock.armor;
+
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.equipment.ArmorType;
+
+public class HorseLimoniteArmor extends ModHorseArmor
+{
+    protected HorseLimoniteArmor(ModArmorMaterial material, ArmorType equipmentSlot, Properties properties)
+    {
+        super(material, equipmentSlot, properties);
+    }
+
+    @Override
+    public boolean canWalkOnPowderedSnow(ItemStack stack, LivingEntity wearer)
+    {
+        return true;
+    }
+}
---
+++ src/main/java/com/pekar/angelblock/events/AnimalManager.java
@@ -0,0 +1,109 @@
+package com.pekar.angelblock.events;
+
+import com.pekar.angelblock.events.animal.IAnimal;
+import com.pekar.angelblock.events.animal.ModAnimal;
+import com.pekar.angelblock.events.armor.IAnimalArmor;
+import net.minecraft.world.entity.TamableAnimal;
+import net.minecraft.world.entity.animal.Animal;
+import net.minecraft.world.entity.animal.equine.AbstractHorse;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.neoforge.event.entity.EntityJoinLevelEvent;
+import net.neoforged.neoforge.event.entity.EntityLeaveLevelEvent;
+import net.neoforged.neoforge.event.entity.living.AnimalTameEvent;
+import net.neoforged.neoforge.event.entity.living.LivingEquipmentChangeEvent;
+
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class AnimalManager implements IAnimalManager, IEventHandler
+{
+    private final Map<UUID, IAnimal> animals = new ConcurrentHashMap<>();
+    private static final IAnimalManager instance;
+
+    static
+    {
+        instance = new AnimalManager();
+    }
+
+    public static IAnimalManager instance()
+    {
+        return instance;
+    }
+
+    @Override
+    public IAnimal getAnimalByUUID(UUID uuid)
+    {
+        return animals.get(uuid);
+    }
+
+    private void addAnimal(Animal entity)
+    {
+        IAnimal animal = new ModAnimal(entity);
+        animals.put(animal.getEntity().getUUID(), animal);
+        animal.updateArmorUsed();
+    }
+
+    @SubscribeEvent
+    public void onEntityJoinLevelEvent(EntityJoinLevelEvent event)
+    {
+        if (event.getLevel().isClientSide()) return;
+
+        var entity = event.getEntity();
+
+        boolean isTameAnimal = (entity instanceof TamableAnimal tamable && tamable.isTame());
+        boolean isTamedHorse = (entity instanceof AbstractHorse horse && horse.isTamed());
+        if (entity instanceof Animal animal && (isTameAnimal || isTamedHorse))
+        {
+            addAnimal(animal);
+        }
+    }
+
+    @SubscribeEvent
+    public void onEntityTameEvent(AnimalTameEvent event)
+    {
+        if (event.getEntity().level().isClientSide()) return;
+
+        var entity = event.getEntity();
+        if (entity instanceof Animal animal)
+        {
+            addAnimal(animal);
+        }
+    }
+
+    @SubscribeEvent
+    public void onEntityLeaveLevelEvent(EntityLeaveLevelEvent event)
+    {
+        if (event.getLevel().isClientSide()) return;
+
+        var entity = event.getEntity();
+        boolean isTameAnimal = (entity instanceof TamableAnimal tamable && tamable.isTame());
+        boolean isTamedHorse = (entity instanceof AbstractHorse horse && horse.isWearingBodyArmor() && horse.isTamed());
+        if (isTameAnimal || isTamedHorse)
+        {
+            animals.remove(entity.getUUID());
+        }
+    }
+
+    @SubscribeEvent
+    public void onEquipmentChangeEvent(LivingEquipmentChangeEvent event)
+    {
+        var entity = event.getEntity();
+        if (entity.level().isClientSide()) return;
+
+        if (entity instanceof Animal)
+        {
+            IAnimal animal = animals.get(entity.getUUID());
+            if (animal == null) return;
+
+            var armorUsed = animal.getArmorTypesUsed();
+            Set<IAnimalArmor> armorAffected = new HashSet<>((Collection<IAnimalArmor>) armorUsed);
+            animal.updateArmorUsed();
+            armorAffected.addAll((Collection<IAnimalArmor>) animal.getArmorTypesUsed());
+
+            for (var armor : armorAffected)
+            {
+                armor.onLivingEquipmentChangeEvent(event);
+            }
+        }
+    }
+}
