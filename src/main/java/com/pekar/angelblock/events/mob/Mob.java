101524b07a8e085d83ba16b1ee2d1566191a3574
PEKAR\Sergey <sergey.pekarchik@gmail.com>
2026-02-25 03:53:16 +0100

    Mob tick optimization

--- src/main/java/com/pekar/angelblock/events/mob/Mob.java
+++ src/main/java/com/pekar/angelblock/events/mob/Mob.java
@@ -8,6 +8,15 @@ import net.minecraft.world.level.Level;
 
 public abstract class Mob implements IMob
 {
+    private int tickCounter = 0;
+
+    @Override
+    public boolean every(int throttling)
+    {
+        tickCounter = (tickCounter + 1) % throttling;
+        return tickCounter == 0;
+    }
+
     public boolean isOverworld()
     {
         return getEntity().level().dimension().identifier().equals(Level.OVERWORLD.identifier());
