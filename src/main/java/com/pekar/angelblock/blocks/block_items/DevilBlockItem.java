13be47cc85267759a328c1a33aa6fa59a6b56a93
PEKAR\Sergey <sergey.pekarchik@gmail.com>
2026-02-26 19:20:31 +0100

    Stack size adjustments. **Bucket of Blue Axolotl** renamed to **Bucket of Rare Axolotl**.

--- src/main/java/com/pekar/angelblock/blocks/block_items/DevilBlockItem.java
+++ src/main/java/com/pekar/angelblock/blocks/block_items/DevilBlockItem.java
@@ -15,7 +15,7 @@ public class DevilBlockItem extends ModBlockItem implements ITooltipProvider
 {
     public DevilBlockItem(Block block, Properties properties)
     {
-        super(block, properties.fireResistant());
+        super(block, properties.fireResistant().stacksTo(1));
     }
 
     @Override
