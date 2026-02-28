51759409cb99e2b79fb2a731fb9aa8b13aad0a60
PEKAR\Sergey <sergey.pekarchik@gmail.com>
2026-02-28 20:02:56 +0100

    **Horse Lymonite Armor** can now be found in *Jungle Temples* or *Woodland Mansions*. The armor tooltips improved.
    (cherry picked from commit 2db7786ece8b81527c016fec4f695dd3bd50986f)
    (cherry picked from commit a036652221e1b3010e853ac4b632ef270fb5fa66)
    (cherry picked from commit d150a2f4e25a7d929a8d9ce88a11fdb3117f3729)
    (cherry picked from commit 1fb4d327d0db1d423a9e105c0c850674fb05b7e2)
    (cherry picked from commit b166445b77dfccb3e8a39424298d4d108ab65c5e)
    (cherry picked from commit c6f1f25289d777f7cfeb63f6aedbc4741e2a93e2)
    (cherry picked from commit b89f56be39c51c854b0acde4e564e74aa5616672)
    (cherry picked from commit 73d6ebaff336180e204aa5414ef30f49c8cac5b3)

--- src/main/java/com/pekar/angelblock/armor/ModHorseArmor.java
+++ src/main/java/com/pekar/angelblock/armor/ModHorseArmor.java
@@ -25,9 +25,10 @@ public class ModHorseArmor extends ModAnimalArmor
         {
             tooltip.ignoreEmptyLines();
 
-            for (int i = 1; i <= 20; i++)
+            for (int i = 0; i <= 20; i++)
             {
                 tooltip.addLine(getDescriptionId(), i)
+                        .styledAs(TextStyle.Notice, i == 0)
                         .styledAs(TextStyle.Header, i == 7)
                         .apply();
             }
