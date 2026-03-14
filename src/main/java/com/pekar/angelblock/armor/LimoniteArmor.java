f91b3813d07ae11cbfa0034e90274c46648be0fc
PEKAR\Sergey <sergey.pekarchik@gmail.com>
2026-03-14 02:58:20 +0100

    Diamite armor now makes the player to sink in powdered snow even when Lymonite, Aerite or Superite boots worn.
    (cherry picked from commit 71649457fa4bea05a2d2d9a003f362c695703d23)
    (cherry picked from commit b3005f1066c4b985882317087df2c69c7feefc2d)
    (cherry picked from commit c2d03c3bf8eec249274372da59f916f7abd65231)

--- src/main/java/com/pekar/angelblock/armor/LimoniteArmor.java
+++ src/main/java/com/pekar/angelblock/armor/LimoniteArmor.java
@@ -1,6 +1,5 @@
 package com.pekar.angelblock.armor;
 
-import com.pekar.angelblock.potions.PotionRegistry;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.ItemStack;
@@ -16,9 +15,12 @@ public class LimoniteArmor extends ModHumanoidArmor
     @Override
     public boolean canWalkOnPowderedSnow(ItemStack stack, LivingEntity wearer)
     {
+        if (isPlayerHeavy(wearer)) return false;
+
         var itemStack = wearer.getItemBySlot(EquipmentSlot.FEET);
         var bootsItem = itemStack.getItem();
         if (!(bootsItem instanceof ModHumanoidArmor boots)) return false;
-        return boots.getArmorType() == PlayerArmorType.LYMONITE && !wearer.hasEffect(PotionRegistry.ARMOR_HEAVY_JUMP_EFFECT);
+
+        return boots.getArmorType() == PlayerArmorType.LYMONITE;
     }
 }
