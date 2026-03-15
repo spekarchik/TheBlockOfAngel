5819c4e5d71ca7b27da1587a5e75ae9578b63a07
PEKAR\Sergey <sergey.pekarchik@gmail.com>
2026-03-15 01:09:54 +0100

    Code optimization for Lymonite Armor

--- src/main/java/com/pekar/angelblock/events/armor/LymoniteArmorController.java
+++ src/main/java/com/pekar/angelblock/events/armor/LymoniteArmorController.java
@@ -205,7 +205,7 @@ public class LymoniteArmorController extends PlayerArmor
                 event.setNewDamage(newDamage);
             }
         }
-        else if (isVulnerable(damageSource) || damageSource.is(DamageTypes.WIND_CHARGE))
+        else if (isVulnerable(damageSource))
         {
             float newDamage = recalculateArmorTypeDamage(event, 0F);
             event.setNewDamage(newDamage);
@@ -217,7 +217,7 @@ public class LymoniteArmorController extends PlayerArmor
     {
         var damageSource = event.getDamageSource();
 
-        if (isVulnerable(damageSource) || damageSource.is(DamageTypes.WIND_CHARGE))
+        if (isVulnerable(damageSource))
         {
             setAdjustedArmorDamage(event, 10F);
         }
