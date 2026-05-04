6a9c2f01905078426ed40ef76af6faddde9b407a
PEKAR\Sergey <sergey.pekarchik@gmail.com>
2026-05-04 17:22:27 +0200

    Reworked **Enhanced Tools** mining behavior in *Enhanced Mode*
    (cherry picked from commit bff8c4d65b1d61c3dbe548694525b80ccc8c98fc)
    (cherry picked from commit 960fbbba1729d43e737335a7d2cccb16a7e5a5c6)

--- src/main/java/com/pekar/angelblock/tools/EnhancedAxe.java
+++ src/main/java/com/pekar/angelblock/tools/EnhancedAxe.java
@@ -6,6 +6,7 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.VegetationBlock;
 import net.minecraft.world.level.block.state.BlockState;
 
@@ -38,9 +39,7 @@ public class EnhancedAxe extends ModAxe
         var block = blockState.getBlock();
         if (!isToolEffective(entityLiving, pos)) return;
 
-        if (blockState.hasBlockEntity() || (blockState != block.defaultBlockState() && !isCompatiblePlant(itemStack, blockState))) return;
-
-        float originHardness = block.defaultDestroyTime();
+        if (canNotBeMinedInGroup(blockState)) return;
 
         final int posX = pos.getX(), posY = pos.getY(), posZ = pos.getZ();
 
@@ -49,7 +48,7 @@ public class EnhancedAxe extends ModAxe
                 for (int z = posZ - 1; z <= posZ + 1; z++)
                 {
                     if (x == posX && y == posY && z == posZ) continue;
-                    onBlockMining(itemStack, level, blockState, originHardness, new BlockPos(x, y, z), entityLiving);
+                    onBlockMining(itemStack, level, new BlockPos(x, y, z), block, entityLiving);
                 }
     }
 
@@ -58,20 +57,27 @@ public class EnhancedAxe extends ModAxe
         return isCorrectToolForDrops(itemStack, blockState) && blockState.getBlock() instanceof VegetationBlock;
     }
 
-    protected void onBlockMining(ItemStack itemStack, Level level, BlockState originBlockState, float originHardness, BlockPos pos, LivingEntity entityLiving)
+    protected void onBlockMining(ItemStack itemStack, Level level, BlockPos pos, Block originBlock, LivingEntity entityLiving)
     {
         var blockState = level.getBlockState(pos);
         var block = blockState.getBlock();
 
-        if (blockState.hasBlockEntity() || (blockState != block.defaultBlockState() && !isCompatiblePlant(itemStack, blockState))) return;
+        if (block != originBlock) return;
+        if (canNotBeMinedInGroup(blockState)) return;
 
         float hardness = block.defaultDestroyTime();
+        float originHardness = originBlock.defaultDestroyTime();
 
         if (hardness <= originHardness && isToolEffective(entityLiving, pos)
-                && (materialProperties.isSafeToBreak(entityLiving, pos) ||  entityLiving.isShiftKeyDown()))
+                && (materialProperties.isSafeToBreak(entityLiving, pos) || entityLiving.isShiftKeyDown()))
         {
-            if (utils.player.destroyBlockByMainHandTool(level, pos, entityLiving, blockState, block))
+            if (utils.player.destroyBlockByMainHandTool(level, pos, entityLiving, blockState))
                 damageMainHandItem(1, entityLiving);
         }
+    }
+
+    private boolean canNotBeMinedInGroup(BlockState blockState)
+    {
+        return blockState.hasBlockEntity();
     }
 }
