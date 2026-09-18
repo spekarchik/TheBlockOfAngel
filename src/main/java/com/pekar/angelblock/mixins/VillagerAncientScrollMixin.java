94a430f468e204cf9385c2a965bb4e378a143bab
PEKAR\Sergey <sergey.pekarchik@gmail.com>
2026-09-18 16:26:27 +0200

    Added the **There Is Hope!** advancement for educating a *Nitwit* with an **Ancient Scroll**.

--- src/main/java/com/pekar/angelblock/mixins/VillagerAncientScrollMixin.java
+++ src/main/java/com/pekar/angelblock/mixins/VillagerAncientScrollMixin.java
@@ -3,6 +3,8 @@ package com.pekar.angelblock.mixins;
 import com.pekar.angelblock.events.VillagerAncientScrollEvents;
 import com.pekar.angelblock.items.ItemRegistry;
 import net.minecraft.server.level.ServerLevel;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.item.ItemEntity;
@@ -46,6 +48,7 @@ public abstract class VillagerAncientScrollMixin extends AbstractVillager
         ci.cancel();
         if (!VillagerAncientScrollEvents.canStartReading(villager)) return;
 
+        Entity itemOwner = itemEntity.getOwner();
         ItemStack scroll = droppedStack.copyWithCount(1);
         this.onItemPickup(itemEntity);
         this.take(itemEntity, 1);
@@ -57,6 +60,10 @@ public abstract class VillagerAncientScrollMixin extends AbstractVillager
         }
 
         this.setItemSlot(EquipmentSlot.MAINHAND, scroll);
-        VillagerAncientScrollEvents.beginReading(villager, scroll);
+        VillagerAncientScrollEvents.beginReading(
+                villager,
+                scroll,
+                itemOwner instanceof ServerPlayer player ? player.getUUID() : null
+        );
     }
 }
