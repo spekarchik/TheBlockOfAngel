d360a444047a1d32811772f32b36dbc57a74937c
PEKAR\Sergey <sergey.pekarchik@gmail.com>
2026-09-22 03:59:21 +0200

    Execute pending level tasks before shutdown or unload

--- src/main/java/com/pekar/angelblock/events/scheduler/LevelScheduler.java
+++ src/main/java/com/pekar/angelblock/events/scheduler/LevelScheduler.java
@@ -4,6 +4,7 @@ import net.minecraft.server.level.ServerLevel;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Predicate;
 
 public abstract class LevelScheduler
 {
@@ -25,5 +26,32 @@ public abstract class LevelScheduler
             task.decrementOrExecute();
             if (task.isCompleted()) iterator.remove();
         }
+    }
+
+    public static void executeAll(ServerLevel level)
+    {
+        executeAll(task -> task.belongsTo(level));
+    }
+
+    public static void executeAll()
+    {
+        executeAll(task -> true);
+    }
+
+    private static void executeAll(Predicate<ILevelScheduledTask> shouldExecute)
+    {
+        var tasksToExecute = new ArrayList<ILevelScheduledTask>();
+        var iterator = tasks.iterator();
+
+        while (iterator.hasNext())
+        {
+            var task = iterator.next();
+            if (!shouldExecute.test(task)) continue;
+
+            tasksToExecute.add(task);
+            iterator.remove();
+        }
+
+        tasksToExecute.forEach(ILevelScheduledTask::execute);
     }
 }
