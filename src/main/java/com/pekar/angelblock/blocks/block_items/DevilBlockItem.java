c71e0ba78b4f7c74ce51488e7c494f6d4d02ef64
PEKAR\Sergey <sergey.pekarchik@gmail.com>
2026-09-22 22:35:49 +0200

    Expanded The Block of Devil's corruption effects
    (cherry picked from commit 5908c9ae0431cb4a2f9de4d647e34a2526e33adc)

--- src/main/java/com/pekar/angelblock/blocks/block_items/DevilBlockItem.java
+++ src/main/java/com/pekar/angelblock/blocks/block_items/DevilBlockItem.java
@@ -42,9 +42,9 @@ public class DevilBlockItem extends ModBlockItem implements ITooltipProvider
         }
         else if (flag.hasAltDown())
         {
-            for (int i = 19; i <= 37; i++)
+            for (int i = 19; i <= 38; i++)
             {
-                tooltip.addLine(getDescriptionId(), i).styledAs(TextStyle.Header, i == 19).styledAs(TextStyle.DarkGray, i >= 34).apply();
+                tooltip.addLine(getDescriptionId(), i).styledAs(TextStyle.Header, i == 19).styledAs(TextStyle.DarkGray, i >= 37).apply();
             }
 
             tooltip.addEmptyLine();
